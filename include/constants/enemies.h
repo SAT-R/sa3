46a8e77b26ce1f29492b862a34ada43074db28c8
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-03-15 19:28:50 +0100

    Add a few shared /include files

--- include/constants/enemies.h
+++ include/constants/enemies.h
@@ -1,28 +1,39 @@
 #ifndef GUARD_CONSTANTS_ENEMIES_H
 #define GUARD_CONSTANTS_ENEMIES_H
 
-#define ENEMY__MON          0 // Jumping monkey
-#define ENEMY__KIKI         1 // Monkey on tree
-#define ENEMY__BUZZER       2
-#define ENEMY__GOHLA        3
-#define ENEMY__KURAKURA     4
-#define ENEMY__KUBINAGA     5
-#define ENEMY__PIKOPIKO     6 // Hammer Clown
-#define ENEMY__BELL         7
-#define ENEMY__YADO         8
-#define ENEMY__CIRCUS       9 // Monkey with explosive
-#define ENEMY__KOURA        10 // Turtle with spring on its back, can have rocket engines.
-#define ENEMY__MADILLO      11
-#define ENEMY__STRAW        12 // Bird
-#define ENEMY__HAMMERHEAD   13 // Shark-shaped platform
-#define ENEMY__SPINNER      14
-#define ENEMY__MOUSE        15
-#define ENEMY__PEN          16 // Penguin
-#define ENEMY__GEJIGEJI     17
-#define ENEMY__BALLOON      18 // Flying Squid
-#define ENEMY__FLICKEY      19
-#define ENEMY__KYURA        20
-#define ENEMY__STAR         21
-#define ENEMY__BULLETBUZZER 22 // A Buzzer with Jet-Engines
+#define ENEMY__SPINNER                     0
+#define ENEMY__AKATENTO                    1
+#define ENEMY__AOTENTO                     2
+#define ENEMY__CONDOR                      3
+#define ENEMY__GEKOGEKO                    4
+#define ENEMY__BUZZER                      5
+#define ENEMY__MINIMOLE                    6
+#define ENEMY__APE_0                       7
+#define ENEMY__APE_1                       8
+#define ENEMY__TAKKON                      9
+#define ENEMY__JOUSUN_0                    10
+#define ENEMY__JOUSUN_1                    11
+#define ENEMY__UUTSUBO_0                   12
+#define ENEMY__UUTSUBO_1                   13
+#define ENEMY__UUTSUBO_2                   14
+#define ENEMY__UUTSUBO_3                   15
+#define ENEMY__GUARD                       16
+#define ENEMY__JUGGLING                    17
+#define ENEMY__GAOGAO                      18
+#define ENEMY__BUBU                        19
+#define ENEMY__GINPE                       20
+#define ENEMY__YADOKK                      21
+#define ENEMY__YUKIGASEN_RIGHT             22
+#define ENEMY__YUKIGASEN_LEFT              23
+#define ENEMY__YUKIGASEN_RIGHT_HI_COOLDOWN 24
+#define ENEMY__YUKIGASEN_LEFT_HI_COOLDOWN  25
+#define ENEMY__HARIISEN                    26
+#define ENEMY__CLAM                        27
+#define ENEMY__KYACCHAA                    28
+#define ENEMY__KAMAKI                      29
+#define ENEMY__GURUGURU                    30
+#define ENEMY__MARUN                       31
+#define ENEMY__MUUKADEN_0                  32
+#define ENEMY__MUUKADEN_1                  33
 
 #endif // GUARD_CONSTANTS_ENEMIES_H
