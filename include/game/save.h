6756ef97d514d0ff351d97d23fbee24545ba793c
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-04-08 11:31:30 +0200

    Formatter...

--- include/game/save.h
+++ include/game/save.h
@@ -49,44 +49,48 @@ struct TimeRecords {
 //       Maybe struc_3000CF0 is the equivalent of struct SaveSectorData ?
 
 // 0x03000530
-struct SaveGame
-{
+struct SaveGame {
     /* 0x00 */ u32 id;
     /* 0x04 */ u16 playerName[MAX_PLAYER_NAME_LENGTH];
-    /* 0x10 */ u8 unlockedCharacters; // 0x10 | Bitfield determining access to playable characters
-    /* 0x11 */ u8 unlockedZones; // 0x11 | Up to the index of this value all Zones are accessible (1 to 9)
-    /* 0x12 */ u8 continueZone;  // 0x12 | Zone the player continues at on startup after character select (0 to 6) 
+    /* 0x10 */ u8
+        unlockedCharacters; // 0x10 | Bitfield determining access to playable characters
+    /* 0x11 */ u8 unlockedZones; // 0x11 | Up to the index of this value all Zones are
+                                 // accessible (1 to 9)
+    /* 0x12 */ u8 continueZone; // 0x12 | Zone the player continues at on startup after
+                                // character select (0 to 6)
     /* 0x13 */ u8 unk13;
     /* 0x14 */ u16 chaoCount[NUM_COURSE_ZONES]; // 0x14 - 0x21 |
-    /* 0x22 */ u8 specialKeys[NUM_COURSE_ZONES]; // 0x22 - 0x28 | Each counter counts for one Zone
+    /* 0x22 */ u8
+        specialKeys[NUM_COURSE_ZONES]; // 0x22 - 0x28 | Each counter counts for one Zone
     /* 0x29 */ u8 zoneBits[9]; // 0x29-0x31 | (struct ZoneCompletion)
     /* 0x32 */ u8 collectedEmeralds; // 0x32 | Stored bitwise: x7654321b
     /* 0x33 */ u8 unk33;
     /* 0x34 */ u16 unk34;
     /* 0x36 */ u8 unk36; /* Live count? */
-    /* 0x37 */ u8 collectedMedals[9][ACTS_PER_ZONE]; // 0x37 | (struct MedalCollection) xxxxxGSBb Medals collected for completion time in each [Zone][Act]
+    /* 0x37 */ u8 collectedMedals[9]
+                                 [ACTS_PER_ZONE]; // 0x37 | (struct MedalCollection)
+                                                  // xxxxxGSBb Medals collected for
+                                                  // completion time in each [Zone][Act]
     /* 0x5B */ u8 unk5B;
     /* 0x5C */ u8 unk5C;
     /* 0x5D */ u8 unk5D;
     /* 0x5E */ u16 unk5E; // Unsure about type
-    
-    
+
     /* 0x60 */ u8 vsWins;
     /* 0x61 */ u8 vsLosses;
     /* 0x62 */ u8 vsDraws;
     /* 0x63 */ u8 unk63; // Vs???
-    
+
     /* 0x64 */ VsRecords vsRecords[10]; // 0x64 | Records in Multiplayer Matches
-    
+
     /* 0x12C */ struct TimeRecords timeRecords;
     /* 0x35C */ struct ButtonConfig buttonConfig;
-    
+
     /* 0x364 */ u8 unk364;
     /* 0x365 */ u8 unk365;
     /* 0x366 */ u8 unk366;
     /* 0x367 */ u8 unk367;
 };
-
 
 #define MULTIPLAYER_RESULT_WIN  0
 #define MULTIPLAYER_RESULT_LOSS 1
