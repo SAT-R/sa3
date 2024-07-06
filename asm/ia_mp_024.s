7515d4552644f2482c2d4646abe17f49337ec014
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-07-06 22:14:28 +0200

    Match CreateEntity_InteractableMP024

--- asm/ia_mp_024.s
+++ asm/ia_mp_024.s
@@ -5,97 +5,11 @@
 .syntax unified
 .arm
 
-	thumb_func_start CreateEntity_InteractableMP024
-CreateEntity_InteractableMP024: @ 0x0804F130
-	push {r4, r5, r6, r7, lr}
-	mov r7, r8
-	push {r7}
-	sub sp, #4
-	adds r5, r0, #0
-	lsls r1, r1, #0x10
-	lsrs r6, r1, #0x10
-	lsls r2, r2, #0x10
-	lsrs r7, r2, #0x10
-	lsls r3, r3, #0x18
-	lsrs r3, r3, #0x18
-	mov r8, r3
-	ldrb r1, [r5, #7]
-	ldr r0, _0804F1D0 @ =gStageData
-	adds r0, #0x98
-	ldr r0, [r0]
-	cmp r0, #0
-	bne _0804F1BA
-	ldr r0, _0804F1D4 @ =gUnknown_03001060
-	adds r0, #0x56
-	ldrb r4, [r0]
-	cmp r4, r1
-	bne _0804F1BA
-	ldr r0, _0804F1D8 @ =sub_804F1EC
-	movs r2, #0x84
-	lsls r2, r2, #6
-	ldr r1, _0804F1DC @ =sub_804F300
-	str r1, [sp]
-	movs r1, #0x3c
-	movs r3, #0
-	bl TaskCreate
-	ldrh r0, [r0, #6]
-	movs r3, #0xc0
-	lsls r3, r3, #0x12
-	adds r3, r0, r3
-	strh r6, [r3, #4]
-	strh r7, [r3, #6]
-	str r5, [r3]
-	ldrb r1, [r5]
-	strb r1, [r3, #0xa]
-	mov r1, r8
-	strb r1, [r3, #0xb]
-	ldrb r1, [r5]
-	lsls r1, r1, #3
-	lsls r2, r6, #8
-	adds r1, r1, r2
-	strh r1, [r3, #0x34]
-	ldrb r1, [r5, #1]
-	lsls r1, r1, #3
-	lsls r2, r7, #8
-	adds r1, r1, r2
-	strh r1, [r3, #0x36]
-	ldr r2, _0804F1E0 @ =0x03000038
-	adds r1, r0, r2
-	strb r4, [r1]
-	ldr r1, _0804F1E4 @ =0x0300000C
-	adds r0, r0, r1
-	ldr r4, _0804F1E8 @ =gCamera
-	ldr r2, [r4]
-	ldrh r1, [r3, #0x34]
-	subs r1, r1, r2
-	strh r1, [r0, #0x10]
-	ldr r2, [r4, #4]
-	ldrh r1, [r3, #0x36]
-	subs r1, r1, r2
-	strh r1, [r0, #0x12]
-	bl sub_804F314
-_0804F1BA:
-	movs r2, #2
-	rsbs r2, r2, #0
-	adds r0, r2, #0
-	strb r0, [r5]
-	add sp, #4
-	pop {r3}
-	mov r8, r3
-	pop {r4, r5, r6, r7}
-	pop {r0}
-	bx r0
-	.align 2, 0
-_0804F1D0: .4byte gStageData
-_0804F1D4: .4byte gUnknown_03001060
-_0804F1D8: .4byte sub_804F1EC
-_0804F1DC: .4byte sub_804F300
-_0804F1E0: .4byte 0x03000038
-_0804F1E4: .4byte 0x0300000C
-_0804F1E8: .4byte gCamera
+.if 0
+.endif
 
-	thumb_func_start sub_804F1EC
-sub_804F1EC: @ 0x0804F1EC
+	thumb_func_start Task_InteractableMP024
+Task_InteractableMP024: @ 0x0804F1EC
 	push {r4, r5, r6, r7, lr}
 	mov r7, sb
 	mov r6, r8
@@ -229,8 +143,8 @@ _0804F2EE:
 	.align 2, 0
 _0804F2FC: .4byte gCamera
 
-	thumb_func_start sub_804F300
-sub_804F300: @ 0x0804F300
+	thumb_func_start TaskDestructor_InteractableMP024
+TaskDestructor_InteractableMP024: @ 0x0804F300
 	push {lr}
 	ldrh r0, [r0, #6]
 	movs r1, #0xc0
