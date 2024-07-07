7e18533cbcf6755dc71b6a80dcb2cacb74ce2d84
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-07-07 21:20:19 +0200

    Document data1.s pointers

--- data/data1.s
+++ data/data1.s
@@ -5,12 +5,19 @@
 
     .global gUnknown_082B483C
 gUnknown_082B483C:
-    .incbin "baserom.gba", 0x002B483C, 0x38
+    .4byte gUnknown_03006870
+    .4byte sub_80BC7C0, sub_80BCC6C, sub_80BCCBC, sub_80BCD08
+    .4byte sub_80BCD14, sub_80BCD20, sub_80C66DC, sub_80BCCC8
+    .4byte sub_80BCCD8, sub_80BCCE8, sub_80BCCF8, sub_80BCD38
+    .4byte sub_80BCD48
 
     .global gUnknown_082B4874
 gUnknown_082B4874:
-    .incbin "baserom.gba", 0x002B4874, 0x10
+    .4byte sub_80BC9DC, sub_80BEE38, sub_80BE690, sub_80BD4A8
 
     .global gUnknown_082B4884
 gUnknown_082B4884:
-    .incbin "baserom.gba", 0x002B4884, 0x30
+    .4byte sub_80BE2A4, sub_80BE334, sub_80BF13C
+    .4byte sub_80BF148, sub_80BF158, sub_80BE3C8, sub_80BF16C
+    .4byte sub_80BF188, sub_80BF194, sub_80BF1AC, sub_80BF1B4
+    .4byte sub_80BF1C0
