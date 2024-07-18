7d214e1a124648816d1b15331c7ca3ece2a85e73
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-07-18 23:29:32 +0200

    Match Task_Pendulum

--- src/game/interactables/pendulum.c
+++ src/game/interactables/pendulum.c
@@ -28,13 +28,13 @@ typedef struct {
 typedef struct {
     /* 0x000 */ SpriteBase base;
     /* 0x00C */ PendSprite sprBalls[BALLS_PER_PEND * PEND_COUNT];
-    /* 0x064 */ PendSprite sprSegments[PEND_COUNT][SEGMENTS_PER_PEND];
+    /* 0x064 */ PendSprite sprSegments[SEGMENTS_PER_PEND * PEND_COUNT];
     /* 0x16C */ u16 activePend;
     /* 0x16E */ s16 qSwingPos[PEND_COUNT]; // -Q(24./256.) to -Q(1)
     /* 0x172 */ s16 swingForce[PEND_COUNT]; // grows/shrinks by 2 if the associated pend is active
     /* 0x176 */ u8 filler176[0x6];
     /* 0x17C */ s32 worldX;
-    /* 0x180 */ s32 worldY;
+    /* 0x17E */ s32 worldY;
 } Pendulum; /* size: 0x184 */
 
 void Task_Pendulum(void);
@@ -67,29 +67,24 @@ void CreateEntity_Pendulum(MapEntity *me, u16 regionX, u16 regionY, u8 id)
     sub_804BEE4(pend);
 }
 
-#if 0
-// (96.68%) https://decomp.me/scratch/vQ5OR
 void Task_Pendulum(void)
 {
     Pendulum *pend = TASK_DATA(gCurTask);
+    PendSprite *ps;
     s16 sp04[PEND_COUNT];
     s16 sp08[PEND_COUNT];
     s16 worldX, worldY;
-    s16 i, j, pid;
+    s16 i, j;
 
-    if(pend->activePend == PEND_LEFT)
-    {
-        if(pend->qSwingPos[PEND_LEFT] <= -PEND_IDLE_POS)
-        {
+    if (pend->activePend == PEND_LEFT) {
+        if (pend->qSwingPos[PEND_LEFT] <= -PEND_IDLE_POS) {
             pend->swingForce[PEND_LEFT] += PEND_FORCE_DELTA;
             pend->qSwingPos[PEND_LEFT] += 1 + (pend->swingForce[PEND_LEFT] >> 5);
 
-            if(pend->qSwingPos[PEND_LEFT] < -Q(1.0))
-            {
+            if (pend->qSwingPos[PEND_LEFT] < -Q(1.0)) {
                 pend->qSwingPos[PEND_LEFT] = -Q(1.0);
             }
-        }
-        else {
+        } else {
             // _0804BC38
             pend->activePend = PEND_RIGHT;
             pend->swingForce[PEND_LEFT] = 0;
@@ -97,15 +92,13 @@ void Task_Pendulum(void)
             pend->qSwingPos[PEND_LEFT] = -PEND_IDLE_POS;
             sub_8003DF0(SE_PENDULUM_HIT);
         }
-    }
-    else {
+    } else {
         // _0804BC68
-        if(pend->qSwingPos[PEND_RIGHT] >= +PEND_IDLE_POS)
-        {
+        if (pend->qSwingPos[PEND_RIGHT] >= +PEND_IDLE_POS) {
             pend->swingForce[PEND_RIGHT] -= PEND_FORCE_DELTA;
             pend->qSwingPos[PEND_RIGHT] += (pend->swingForce[PEND_RIGHT] >> 5);
 
-            if( pend->qSwingPos[PEND_RIGHT] > +Q(1.0)) {
+            if (pend->qSwingPos[PEND_RIGHT] > +Q(1.0)) {
                 pend->qSwingPos[PEND_RIGHT] = +Q(1.0);
             }
         } else {
@@ -119,11 +112,10 @@ void Task_Pendulum(void)
     }
     // _0804BCBC
 
-    for(i = 0; i < PEND_COUNT; i++)
-    {
+    for (i = 0; i < PEND_COUNT; i++) {
         // _0804BCD2
-        PendSprite *ps = &pend->sprBalls[i];
         s32 swingPos;
+        ps = &pend->sprBalls[i];
 
         sp04[i] = ps->dx;
         sp08[i] = ps->dy;
@@ -135,12 +127,11 @@ void Task_Pendulum(void)
         sp08[i] -= ps->dy;
 
         // __0804BD3A
-        for(j = 0; j < SEGMENTS_PER_PEND; j++)
-        {
+        for (j = 0; j < SEGMENTS_PER_PEND; j++) {
             // _0804BD40
             s32 sinValue;
             ps = &pend->sprSegments[i * SEGMENTS_PER_PEND + j];
-            
+
             ps->dx = ((SIN(pend->qSwingPos[i] & ONE_CYCLE) >> 9) * (j + 1));
             ps->dy = ((COS(pend->qSwingPos[i] & ONE_CYCLE) >> 9) * (j + 1));
         }
@@ -150,32 +141,26 @@ void Task_Pendulum(void)
     worldX = pend->worldX;
     worldY = pend->worldY;
 
-    for(pid = 0; pid < NUM_SINGLE_PLAYER_CHARS; pid++)
-    {
+    for (j = 0; j < NUM_SINGLE_PLAYER_CHARS; j++) {
         // _0804BDB0
         Player *p;
 
-        if (pid == 0) {
+        if (j == 0) {
             p = &gPlayers[gStageData.charId];
         } else {
             p = &gPlayers[p->charFlags.partnerIndex];
         }
 
-        if(((p->charFlags.someIndex == 1)
-        ||  (p->charFlags.someIndex == 2)
-        ||  (p->charFlags.someIndex == 4))
-        &&  !sub_802C0D4(p))
-        {
+        if (((p->charFlags.someIndex == 1) || (p->charFlags.someIndex == 2) || (p->charFlags.someIndex == 4)) && !sub_802C0D4(p)) {
             // _0804BE0C
-            for(i = 0; i < PEND_COUNT; i++)
-            {
+            for (i = 0; i < PEND_COUNT; i++) {
                 // _0804BE22
                 u32 mask;
-                PendSprite *ps = &pend->sprBalls[i];
-                Sprite *s = &ps->s;
+                Sprite *s;
+                ps = &pend->sprBalls[i];
+                s = &ps->s;
 
-                if((p->moveState & MOVESTATE_20) && (p->spr6C == s))
-                {
+                if ((p->moveState & MOVESTATE_20) && (p->spr6C == s)) {
                     s32 val;
                     p->qWorldX -= Q(sp04[i]);
                     val = p->qWorldY + Q(4);
@@ -184,18 +169,17 @@ void Task_Pendulum(void)
                 // _0804BE76
 
                 mask = sub_8020950(s, ps->dx + worldX, ps->dy + worldY, p, NULL);
-                if(mask & 0x10000)
-                {
+                if (mask & 0x10000) {
                     s16 qDY = Q(mask & 0x00FF);
                     p->qWorldY += qDY;
                 }
             }
-            
         }
     }
-
 
     // _0804BECA
     sub_804C008();
 }
+
+#if 01
 #endif
