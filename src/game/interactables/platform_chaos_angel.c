7c5ec397b39eadf54d41c098156f7fa682edbcdb
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-08-02 23:12:12 +0200

    Start Task_PlatformChaosAngel

--- src/game/interactables/platform_chaos_angel.c
+++ src/game/interactables/platform_chaos_angel.c
@@ -49,3 +49,90 @@ void CreateEntity_PlatformChaosAngel(MapEntity *me, u16 regionX, u16 regionY, u8
 
     sub_804DBF4(s);
 }
+
+// (72.15%) https://decomp.me/scratch/xovXz
+#if 0
+void Task_PlatformChaosAngel(void) {
+    PlatformCA *platform = TASK_DATA(gCurTask);
+    Sprite *s = &platform->s;
+    u32 r9 = 0;
+    s32 qWorldX, qWorldY;
+    s32 qWorldX32, qWorldY32;
+    s32 qLeft, qTop;
+    s16 i;
+
+    if((gStageData.unk4 == 4) || (platform->unk42 & 0x10)) {
+        MapEntity *me = platform->base.me;
+        u8 unk42 = (platform->unk42 & 0x7);
+
+        platform->unk42 = ((platform->unk42 + 1) & 0xF) | 0x10;
+
+        if((platform->unk42 & 0xF) == 0) {
+            platform->unk43 += 6;
+        }
+
+        if(platform->unk43 > 32) {
+            platform->unk43 = 32;
+        }
+
+        platform->unk40 += platform->unk43 / 4;
+        
+        qWorldX = Q(platform->worldX);
+        qWorldY = Q(platform->worldY);
+    
+        qTop = Q(me->d.sData[1] * TILE_WIDTH);
+        qLeft = Q(me->d.sData[0] * TILE_WIDTH);
+    
+        qWorldX = platform->qWorldX;
+        qWorldY = platform->qWorldY;
+    
+        qLeft = (qLeft * gStageData.unk5E[platform->worldX]) >> 10;
+        qLeft += qWorldX;
+        platform->qWorldX = qLeft;
+    
+        qTop = (qTop * gStageData.unk5E[platform->worldX]) >> 10;
+        qTop += qWorldY;
+        platform->qWorldY = qTop;
+    
+        qWorldX -= qLeft;
+        qWorldY -= qTop;
+    }
+
+    for (i = 0; i < NUM_SINGLE_PLAYER_CHARS; i++) {
+        Player *p;
+
+        if (i == 0) {
+            p = &gPlayers[gStageData.charId];
+        } else {
+            p = &gPlayers[p->charFlags.partnerIndex];
+        }
+
+        if (!sub_802C0D4(p)) {
+            s32 res;
+
+            if ((p->moveState & MOVESTATE_20) && (p->spr6C == s)) {
+                
+                res = sub_80110E8(3, p, NULL, NULL);
+                if (res > 0) {
+                    s16 v = r9;
+                    p->qWorldY += Q(4) + v;
+                    asm("");
+                } else {
+                    p->qWorldY += Q(res);                    
+                }
+            }
+
+            res = sub_8020950(s, I(platform->qWorldX), I(platform->qWorldY), p, NULL);
+
+            if (res & 0x10000) {
+                s16 res16 = Q(res);
+                p->qWorldY += res16;
+                p->qSpeedAirY = 0;
+                platform->unk42 |= 0x10;
+            }
+        }
+    }
+
+    sub_804DB08();
+}
+#endif
