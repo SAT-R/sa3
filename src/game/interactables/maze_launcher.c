4825bbaae533ca8cb21912add044a8ff48e19039
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-10-08 22:37:44 +0200

    Match CreateEntity_MazeLauncherC

--- src/game/interactables/maze_launcher.c
+++ src/game/interactables/maze_launcher.c
@@ -15,8 +15,13 @@ typedef struct {
 
 void Task_MazeLauncher(void);
 void TaskDestructor_MazeLauncher(struct Task *);
+void Task_Interactable137Main(void);
+void TaskDestructor_Interactable137(struct Task *);
+void Task_Interactable138Main(void);
+void TaskDestructor_Interactable138(struct Task *);
 void sub_804D08C(Sprite *, s16 i);
 void sub_804D360(Sprite *, u16 i, s16 a);
+void sub_804D658(Sprite *, s16 i); // like sub_804D08C, but for MazeC
 
 void CreateEntity_MazeLauncherA(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
@@ -54,9 +59,10 @@ void CreateEntity_MazeLauncherA(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 
 // (99.64%) https://decomp.me/scratch/ADZew
 // NOTE: Reg-alloc mismatch r9/sl
-NONMATCH("asm/non_matching/game/interactables/maze_launcher__CreateEntity_MazeLauncherB.inc", void CreateEntity_MazeLauncherB(MapEntity *me, u16 regionX, u16 regionY, u8 id))
+NONMATCH("asm/non_matching/game/interactables/maze_launcher__CreateEntity_MazeLauncherB.inc",
+         void CreateEntity_MazeLauncherB(MapEntity *me, u16 regionX, u16 regionY, u8 id))
 {
-    struct Task *t = TaskCreate(Task_MazeLauncher, sizeof(MazeLauncher), 0x2100, 0, TaskDestructor_MazeLauncher);
+    struct Task *t = TaskCreate(Task_Interactable137Main, sizeof(MazeLauncher), 0x2100, 0, TaskDestructor_Interactable137);
     MazeLauncher *maze = TASK_DATA(t);
     Sprite *s;
     s16 i;
@@ -73,7 +79,7 @@ NONMATCH("asm/non_matching/game/interactables/maze_launcher__CreateEntity_MazeLa
     }
 
     maze->unk3C = i;
-    
+
     for (i = 0; i < 4; i++) {
         if (((me->d.uData[4]) >> (i + 4)) & 0x1)
             break;
@@ -92,3 +98,36 @@ NONMATCH("asm/non_matching/game/interactables/maze_launcher__CreateEntity_MazeLa
     sub_804D360(s, maze->unk3C, maze->unk3D);
 }
 END_NONMATCH
+
+void CreateEntity_MazeLauncherC(MapEntity *me, u16 regionX, u16 regionY, u8 id)
+{
+    struct Task *t = TaskCreate(Task_Interactable138Main, sizeof(MazeLauncher), 0x2100, 0, TaskDestructor_Interactable138);
+    MazeLauncher *maze = TASK_DATA(t);
+    Sprite *s;
+    s16 i;
+
+    maze->base.regionX = regionX;
+    maze->base.regionY = regionY;
+    maze->base.me = me;
+    maze->base.spriteX = me->x;
+    maze->base.id = id;
+
+    for (i = 0; i < 4; i++) {
+        if ((me->d.uData[4] >> i) & 0x1)
+            break;
+    }
+
+    maze->unk3C = i;
+    maze->unk3E = 0;
+
+    maze->unk40 = TO_WORLD_POS(me->x, regionX);
+    maze->unk42 = TO_WORLD_POS(me->y, regionY);
+
+    s = &maze->s;
+    s->x = maze->unk40 - gCamera.x;
+    s->y = maze->unk42 - gCamera.y;
+
+    SET_MAP_ENTITY_INITIALIZED(me);
+
+    sub_804D658(s, i);
+}
