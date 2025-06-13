014b4618cdb588f7ef6b64863eb7a84c33992ae0
macabeus <bruno.macabeus@gmail.com>
2025-06-13 01:01:33 +0100

    Match Enemy Bubu

--- asm/enemy_bu_bu.s
+++ asm/enemy_bu_bu.s
@@ -5,105 +5,6 @@
 .syntax unified
 .arm
 
-	thumb_func_start CreateEntity_BuBu
-CreateEntity_BuBu: @ 0x0805EBFC
-	push {r4, r5, r6, r7, lr}
-	mov r7, r8
-	push {r7}
-	sub sp, #8
-	mov r8, r0
-	adds r5, r1, #0
-	adds r6, r2, #0
-	adds r4, r3, #0
-	lsls r5, r5, #0x10
-	lsrs r5, r5, #0x10
-	lsls r6, r6, #0x10
-	lsrs r6, r6, #0x10
-	lsls r4, r4, #0x18
-	lsrs r4, r4, #0x18
-	ldr r0, _0805EC84 @ =sub_805ED48
-	movs r2, #0x84
-	lsls r2, r2, #6
-	ldr r1, _0805EC88 @ =sub_805F2AC
-	str r1, [sp]
-	movs r1, #0x5c
-	movs r3, #0
-	bl TaskCreate
-	ldrh r1, [r0, #6]
-	movs r0, #0xc0
-	lsls r0, r0, #0x12
-	adds r7, r1, r0
-	movs r2, #0
-	movs r0, #0
-	strh r0, [r7, #0xe]
-	strh r0, [r7, #0x10]
-	strb r2, [r7, #6]
-	mov r0, r8
-	str r0, [r7]
-	ldrb r0, [r0]
-	strb r0, [r7, #5]
-	strb r4, [r7, #4]
-	strh r5, [r7, #0xa]
-	strh r6, [r7, #0xc]
-	mov r3, r8
-	ldrb r1, [r3]
-	lsls r1, r1, #0xb
-	str r1, [r7, #0x1c]
-	ldrb r0, [r3, #1]
-	lsls r0, r0, #0xb
-	str r0, [r7, #0x20]
-	str r1, [r7, #0x14]
-	str r0, [r7, #0x18]
-	movs r0, #0x5a
-	strh r0, [r7, #0xe]
-	movs r0, #3
-	ldrsb r0, [r3, r0]
-	lsls r0, r0, #0xb
-	adds r1, r1, r0
-	str r1, [r7, #0x24]
-	ldrb r0, [r3, #5]
-	lsls r0, r0, #0xb
-	adds r1, r1, r0
-	str r1, [r7, #0x28]
-	strb r2, [r7, #8]
-	ldrb r1, [r3, #7]
-	movs r0, #8
-	ands r0, r1
-	cmp r0, #0
-	beq _0805EC8C
-	movs r0, #0xff
-	b _0805EC8E
-	.align 2, 0
-_0805EC84: .4byte sub_805ED48
-_0805EC88: .4byte sub_805F2AC
-_0805EC8C:
-	movs r0, #1
-_0805EC8E:
-	strb r0, [r7, #9]
-	add r1, sp, #4
-	movs r0, #0
-	strh r0, [r1]
-	adds r1, r7, #0
-	adds r1, #0x58
-	ldr r2, _0805ECC0 @ =0x01000002
-	add r0, sp, #4
-	bl CpuSet
-	adds r0, r7, #0
-	bl sub_805ECC4
-	movs r1, #2
-	rsbs r1, r1, #0
-	adds r0, r1, #0
-	mov r3, r8
-	strb r0, [r3]
-	add sp, #8
-	pop {r3}
-	mov r8, r3
-	pop {r4, r5, r6, r7}
-	pop {r0}
-	bx r0
-	.align 2, 0
-_0805ECC0: .4byte 0x01000002
-
 	thumb_func_start sub_805ECC4
 sub_805ECC4: @ 0x0805ECC4
 	push {r4, r5, r6, lr}
@@ -801,6 +702,8 @@ _0805F1FA:
 _0805F1FC:
 	pop {r1}
 	bx r1
+
+@@
 
 	thumb_func_start sub_805F200
 sub_805F200: @ 0x0805F200
