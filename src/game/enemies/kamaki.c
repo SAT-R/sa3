3a441e289c9136163caeb60a9c0e6f58d027146a
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-11-24 23:13:19 +0100

    Match sub_80648EC and Task_Kamaki

--- src/game/enemies/kamaki.c
+++ src/game/enemies/kamaki.c
@@ -1,11 +1,13 @@
 #include "global.h"
 #include "core.h"
+#include "malloc_vram.h"
 #include "game/camera.h"
 #include "game/entity.h"
+#include "game/stage.h"
 
 typedef struct Kamaki {
     /* 0x00 */ MapEntity *me;
-    /* 0x08 */ s8 unk4;
+    /* 0x08 */ u8 unk4;
     /* 0x05 */ u8 id;
     /* 0x06 */ u8 meX;
     /* 0x07 */ u8 unk7;
@@ -13,8 +15,7 @@ typedef struct Kamaki {
     /* 0x0A */ u16 region[2];
     /* 0x0E */ u16 unkE[2];
     /* 0x12 */ s16 unk12;
-    /* 0x14 */ s32 unkX;
-    /* 0x18 */ s32 unkY;
+    /* 0x14 */ Vec2_32 qUnk;
     /* 0x1C */ Vec2_32 qPos;
     /* 0x24 */ s32 qLeft;
     /* 0x28 */ s32 qRight;
@@ -23,7 +24,15 @@ typedef struct Kamaki {
 
 void Task_Kamaki(void);
 void sub_80648EC(Kamaki *enemy);
+void sub_8064A60(void);
+void sub_8064C18(s32 arg0, s32 arg1, u16 arg2, u16 arg3, u32 arg4, s32 arg5);
+void sub_805CD70(Vec2_32 *qVal, Vec2_32 *param1, u16 region[2], s8 *param3);
+void sub_805CE14(Vec2_32 *qVal, Vec2_32 *param1, u16 region[2], s8 *param3);
+bool32 sub_8065104(Kamaki *enemy);
+AnimCmdResult sub_8065084(Kamaki *enemy);
 void TaskDestructor_Kamaki(struct Task *t);
+
+extern const TileInfo2 gUnknown_080D215C[2];
 
 void CreateEntity_Kamaki(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
@@ -36,9 +45,9 @@ void CreateEntity_Kamaki(MapEntity *me, u16 regionX, u16 regionY, u8 id)
     enemy->region[1] = regionY;
     enemy->qPos.x = Q(me->x * TILE_WIDTH);
     enemy->qPos.y = Q(me->y * TILE_WIDTH);
-    enemy->unkX = enemy->qPos.x;
-    enemy->unkY = enemy->qPos.y;
-    enemy->qLeft = enemy->unkX + Q(me->d.sData[0] * TILE_WIDTH);
+    enemy->qUnk.x = enemy->qPos.x;
+    enemy->qUnk.y = enemy->qPos.y;
+    enemy->qLeft = enemy->qUnk.x + Q(me->d.sData[0] * TILE_WIDTH);
     enemy->qRight = enemy->qLeft + Q(me->d.uData[2] * TILE_WIDTH);
     enemy->unk7 = 0;
     enemy->unkE[0] = 0;
@@ -61,4 +70,71 @@ void CreateEntity_Kamaki(MapEntity *me, u16 regionX, u16 regionY, u8 id)
     sub_80648EC(enemy);
 
     SET_MAP_ENTITY_INITIALIZED(me);
+}
+
+void sub_80648EC(Kamaki *enemy)
+{
+    u8 *vram = VramMalloc(0x10U);
+    Sprite2 *s;
+
+    s = &enemy->s;
+    s->tiles = vram;
+    s->anim = gUnknown_080D215C[0].anim;
+    s->variant = gUnknown_080D215C[0].variant;
+    s->prevVariant = -1;
+    s->x = TO_WORLD_POS_RAW(I(enemy->qPos.x), enemy->region[0]) - gCamera.x;
+    s->y = TO_WORLD_POS_RAW(I(enemy->qPos.y), enemy->region[1]) - gCamera.y;
+    s->oamFlags = 0x480;
+    s->animCursor = 0;
+    s->qAnimDelay = 0;
+    s->animSpeed = 0x10;
+    s->palId = 0;
+    s->frameFlags = 0x1000;
+
+    if (enemy->dir < 0) {
+        s->frameFlags |= 0x400;
+        s->frameFlags |= 0x1000;
+    }
+
+    if (enemy->unk4 != 0) {
+        s->frameFlags |= 0x800;
+    }
+    s->hitboxes[0].index = -1;
+    UpdateSpriteAnimation((Sprite *)s);
+}
+
+void Task_Kamaki(void)
+{
+    u16 temp_r0;
+    AnimCmdResult temp_r2;
+
+    Kamaki *enemy = TASK_DATA(gCurTask);
+    Sprite2 *s = &enemy->s;
+
+    if (enemy->unk4 != 0) {
+        sub_805CE14(&enemy->qPos, &enemy->qUnk, enemy->region, &enemy->unk7);
+    } else {
+        sub_805CD70(&enemy->qPos, &enemy->qUnk, enemy->region, &enemy->unk7);
+    }
+    if (sub_8065104(enemy) == 1) {
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    temp_r2 = sub_8065084(enemy);
+    if ((gStageData.unk4 != 1) && (gStageData.unk4 != 2) && (gStageData.unk4 != 4)) {
+        if (temp_r2 == 0) {
+            enemy->unkE[0] = temp_r2;
+            s->prevVariant = -1;
+        }
+
+        if (++enemy->unkE[1] > 0x77U) {
+            Sprite2 *s = &enemy->s;
+            s->anim = gUnknown_080D215C[1].anim;
+            s->variant = gUnknown_080D215C[1].variant;
+            s->prevVariant = -1;
+            enemy->unkE[1] = 0;
+            gCurTask->main = sub_8064A60;
+        }
+    }
 }
