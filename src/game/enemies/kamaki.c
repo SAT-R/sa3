7ae6c33cae1b14c133caf342c0b8b82cc96a3e60
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-11-25 17:10:35 +0100

    Match sub_8064B54 and fakematch sub_8064C18

--- src/game/enemies/kamaki.c
+++ src/game/enemies/kamaki.c
@@ -22,11 +22,33 @@ typedef struct Kamaki {
     /* 0x2C */ Sprite2 s;
 } Kamaki;
 
+typedef struct KamakiProj {
+    /* 0x00 */ u8 unk0;
+    /* 0x01 */ u8 unk1;
+    /* 0x02 */ u16 unk2;
+    /* 0x04 */ u16 unk4;
+    /* 0x06 */ u16 unk6;
+    /* 0x08 */ u16 unk8;
+    /* 0x0A */ s16 unkA;
+    /* 0x0C */ s16 unkC;
+    /* 0x0E */ u16 unkE;
+    /* 0x10 */ s32 qLeft;
+    /* 0x14 */ s32 qTop;
+    /* 0x18 */ s32 qRight;
+    /* 0x1C */ s32 qBottom;
+    /* 0x20 */ u8 unk20[0x18];
+    /* 0x34 */ Sprite2 s;
+    /* 0x64 */ Sprite2 s2;
+} KamakiProj; /* 0x98 */
+
 void Task_Kamaki(void);
 void sub_80648EC(Kamaki *enemy);
 void sub_8064A60(void);
 void sub_8064B54(void);
-void sub_8064C18(s32 arg0, s32 arg1, u16 arg2, u16 arg3, u32 arg4, s32 arg5);
+void Task_8064E10(void);
+void TaskDestructor_80651BC(struct Task *t);
+void sub_8064C18(s32 arg0, s32 arg1, u16 arg2, u16 arg3, u8 arg4, u8 arg5);
+void sub_8064D04(KamakiProj *proj);
 void sub_805CD70(Vec2_32 *qVal, Vec2_32 *param1, u16 region[2], s8 *param3);
 void sub_805CE14(Vec2_32 *qVal, Vec2_32 *param1, u16 region[2], s8 *param3);
 bool32 sub_8065104(Kamaki *enemy);
@@ -34,6 +56,7 @@ AnimCmdResult sub_8065084(Kamaki *enemy);
 void TaskDestructor_Kamaki(struct Task *t);
 
 extern const TileInfo2 gUnknown_080D215C[4];
+extern const s16 gUnknown_080D217C[2];
 
 void CreateEntity_Kamaki(MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
@@ -161,7 +184,7 @@ void sub_8064A60(void)
 
     if ((gStageData.unk4 != 1) && (gStageData.unk4 != 2) && (gStageData.unk4 != 4)) {
         if (++enemy->unkE[1] == 0x61) {
-            s32 v = (u32)(-(enemy->s.frameFlags & 0x400)) >> 0x1F;
+            u8 v = (u32)(-(enemy->s.frameFlags & 0x400)) >> 0x1F;
             sub_8064C18(enemy->qPos.x, enemy->qPos.y, enemy->region[0], enemy->region[1], v, enemy->unk4);
         }
         if (acmdRes == ACMD_RESULT__ENDED) {
@@ -173,4 +196,78 @@ void sub_8064A60(void)
             gCurTask->main = sub_8064B54;
         }
     }
+}
+
+void sub_8064B54(void)
+{
+    Sprite2 *s;
+    AnimCmdResult acmdRes;
+    u16 temp_r0;
+
+    Kamaki *enemy = TASK_DATA(gCurTask);
+
+    if (enemy->unk4 != 0) {
+        sub_805CE14(&enemy->qPos, &enemy->qUnk, enemy->region, (s8 *)&enemy->unk7);
+    } else {
+        sub_805CD70(&enemy->qPos, &enemy->qUnk, enemy->region, (s8 *)&enemy->unk7);
+    }
+    if (sub_8065104(enemy) == 1) {
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    acmdRes = sub_8065084(enemy);
+
+    if ((gStageData.unk4 != 1) && (gStageData.unk4 != 2) && (gStageData.unk4 != 4)) {
+        if (acmdRes == ACMD_RESULT__ENDED) {
+            s = &enemy->s;
+            s->anim = gUnknown_080D215C[0].anim;
+            s->variant = gUnknown_080D215C[0].variant;
+            s->prevVariant = -1;
+            enemy->unkE[0] = 0;
+            gCurTask->main = Task_Kamaki;
+        }
+    }
+}
+
+void sub_8064C18(s32 arg0, s32 arg1, u16 arg2, u16 arg3, u8 arg4, u8 arg5)
+{
+    s32 temp_r1_2;
+    KamakiProj *proj;
+#ifndef NON_MATCHING
+    register s32 var_r0 asm("r0");
+#else
+    s32 var_r0;
+#endif
+
+    proj = TASK_DATA(TaskCreate(Task_8064E10, sizeof(KamakiProj), 0x4040U, 0U, TaskDestructor_80651BC));
+    proj->unk2 = arg2;
+    proj->unk4 = arg3;
+    proj->unkC = +Q(5);
+    proj->unkA = -Q(5);
+    proj->unkE = 0;
+    proj->unk6 = 0;
+    proj->unk8 = 0;
+    proj->unk0 = arg5;
+    temp_r1_2 = Q(gUnknown_080D217C[arg4]);
+    var_r0 = (temp_r1_2 - Q(8));
+    proj->qLeft = var_r0 + arg0;
+    var_r0 = Q(8);
+    temp_r1_2 += var_r0;
+    arg0 += temp_r1_2;
+    proj->qRight = arg0;
+
+    if (arg5 != 0) {
+        var_r0 = +Q(32);
+        var_r0 += arg1;
+    } else {
+        var_r0 = -Q(32);
+        var_r0 += arg1;
+    }
+
+    proj->qTop = var_r0;
+    proj->qBottom = var_r0;
+    CpuFill16(0, &proj->s.hitboxes[1].b, sizeof(proj->s.hitboxes[1].b));
+    CpuFill16(0, &proj->s2.hitboxes[1].b, sizeof(proj->s.hitboxes[1].b));
+    sub_8064D04(proj);
 }
