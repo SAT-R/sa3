b1fad4900e06f8597edf8feded2177a213733508
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-11-26 21:28:04 +0100

    Match sub_805D188 and sub_805D298

--- src/game/enemies/uutsubo.c
+++ src/game/enemies/uutsubo.c
@@ -1,9 +1,16 @@
 #include "global.h"
 #include "core.h"
+#include "malloc_vram.h"
 #include "game/camera.h"
 #include "game/entity.h"
 
 #define NUM_BODY_SEGMENTS 5
+typedef enum UutsuboType {
+    UUTYPE_A,
+    UUTYPE_B,
+    UUTYPE_C,
+    UUTYPE_D,
+} UutsuboType;
 
 typedef struct Uutsubo {
     /* 0x00 */ MapEntity *me;
@@ -19,8 +26,7 @@ typedef struct Uutsubo {
     /* 0x30 */ Vec2_32 qBodyPositions[NUM_BODY_SEGMENTS];
     /* 0x58 */ Vec2_32 qHeadPos;
     /* 0x60 */ Vec2_32 qPos;
-    /* 0x68 */ u16 unk68;
-    /* 0x6A */ u8 filler6A[0xA];
+    /* 0x68 */ SpriteTransform tf;
     /* 0x74 */ Sprite2 s;
     /* 0xA4 */ Sprite2 s2;
 } Uutsubo; /* 0xD4 */
@@ -30,6 +36,11 @@ void CreateEntity_Uutsubo_1(MapEntity *me, u16 regionX, u16 regionY, u8 id);
 void CreateEntity_Uutsubo_2(MapEntity *me, u16 regionX, u16 regionY, u8 id);
 void CreateEntity_Uutsubo_3(MapEntity *me, u16 regionX, u16 regionY, u8 id);
 void CreateEntity_Uutsubo_Shared(Uutsubo *enemy, MapEntity *me, u16 regionX, u16 regionY, u8 id);
+
+void sub_805D188(Uutsubo *enemy, u8 type);
+void sub_805D298(Sprite2 *arg0, SpriteTransform *tf, u8 type);
+
+extern const TileInfo2 gUnknown_080D1F4C[2];
 
 void CreateEntity_Uutsubo_Shared(Uutsubo *enemy, MapEntity *me, u16 regionX, u16 regionY, u8 id)
 {
@@ -47,7 +58,7 @@ void CreateEntity_Uutsubo_Shared(Uutsubo *enemy, MapEntity *me, u16 regionX, u16
     enemy->unk16[0] = 0;
     enemy->unk16[1] = 0;
     enemy->unk24 = 0;
-    enemy->unk68 = 0;
+    enemy->tf.rotation = 0;
 
     for (i = 0; i < NUM_BODY_SEGMENTS; i++) {
         enemy->qBodyPositions[i].x = enemy->qHeadPos.x;
@@ -61,4 +72,79 @@ void CreateEntity_Uutsubo_Shared(Uutsubo *enemy, MapEntity *me, u16 regionX, u16
 
     CpuFill16(0, &enemy->s.hitboxes[0].b, sizeof(enemy->s.hitboxes[1].b));
     CpuFill16(0, &enemy->s2.hitboxes[0].b, sizeof(enemy->s.hitboxes[1].b));
+}
+
+void sub_805D188(Uutsubo *enemy, u8 type)
+{
+    SpriteTransform *tf;
+    Uutsubo *sp4;
+    Sprite2 *s;
+    u8 *vram;
+
+    tf = &enemy->tf;
+    vram = VramMalloc(0x20U);
+    s = &enemy->s2;
+    s->tiles = vram;
+    vram += (gUnknown_080D1F4C[1].numTiles * TILE_SIZE_4BPP);
+    s->anim = gUnknown_080D1F4C[1].anim;
+    s->variant = gUnknown_080D1F4C[1].variant;
+    s->prevVariant = -1;
+    s->x = TO_WORLD_POS_RAW(I(enemy->qPos.x), enemy->region[0]) - gCamera.x;
+    s->y = TO_WORLD_POS_RAW(I(enemy->qPos.y), enemy->region[1]) - gCamera.y;
+    s->oamFlags = SPRITE_OAM_ORDER(18);
+    s->animCursor = 0;
+    s->qAnimDelay = 0;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = -1;
+    sub_805D298(s, tf, type);
+    UpdateSpriteAnimation((Sprite *)s);
+
+    s = &enemy->s;
+    enemy->s.tiles = vram;
+    s->anim = gUnknown_080D1F4C[0].anim;
+    s->variant = gUnknown_080D1F4C[0].variant;
+    s->prevVariant = -1;
+    s->x = TO_WORLD_POS_RAW(I(enemy->qPos.x), enemy->region[0]) - gCamera.x;
+    s->y = TO_WORLD_POS_RAW(I(enemy->qPos.y), enemy->region[1]) - gCamera.y;
+    s->oamFlags = SPRITE_OAM_ORDER(18);
+    s->animCursor = 0;
+    s->qAnimDelay = 0;
+    s->animSpeed = SPRITE_ANIM_SPEED(1.0);
+    s->palId = 0;
+    s->hitboxes[0].index = -1;
+    sub_805D298(s, tf, type);
+    UpdateSpriteAnimation((Sprite *)s);
+}
+
+void sub_805D298(Sprite2 *s, SpriteTransform *tf, u8 type)
+{
+    s->frameFlags = SPRITE_FLAG(PRIORITY, 1);
+
+    switch (type) {
+        case 1:
+            s->frameFlags |= SPRITE_FLAG(PRIORITY, 1);
+            s->frameFlags |= 0x400;
+            break;
+        case 2:
+            s->frameFlags |= (0xA | SPRITE_FLAG_MASK_ROT_SCALE_ENABLE | SPRITE_FLAG_MASK_ROT_SCALE_DOUBLE_SIZE);
+            s->frameFlags |= SPRITE_FLAG(PRIORITY, 1);
+            tf->rotation = 0x100;
+            tf->x = s->x;
+            tf->y = s->y;
+            tf->qScaleX = Q(1);
+            tf->qScaleY = Q(1);
+            TransformSprite((Sprite *)s, (SpriteTransform *)tf);
+            break;
+        case 3:
+            s->frameFlags |= (0xB | SPRITE_FLAG_MASK_ROT_SCALE_ENABLE | SPRITE_FLAG_MASK_ROT_SCALE_DOUBLE_SIZE);
+            s->frameFlags |= SPRITE_FLAG(PRIORITY, 1);
+            tf->rotation = 0x300;
+            tf->x = s->x;
+            tf->y = s->y;
+            tf->qScaleX = Q(1);
+            tf->qScaleY = Q(1);
+            TransformSprite((Sprite *)s, (SpriteTransform *)tf);
+            break;
+    }
 }
