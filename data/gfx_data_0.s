af60e65dd8b41f4be186893f43ba15570667a3d9
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-01-07 15:58:01 +0100

    Convert obj palettes to C

---
+++ data/gfx_data_0.s
@@ -0,0 +1,42 @@
+.include "asm/macros/c_decl.inc"
+.include "asm/macros/portable.inc"
+.include "asm/macros/function.inc"
+.include "constants/gba_constants.inc"
+
+.section .rodata
+
+    .global gUnknown_082B9CD4
+gUnknown_082B9CD4:
+    .incbin "baserom_sa3.gba", 0x002B9CD4, 0x2045C
+
+    .global gUnknown_082DA130
+gUnknown_082DA130:
+    .incbin "baserom_sa3.gba", 0x002DA130, 0x3C0
+
+    .global gTempLabel__AnimationData
+gTempLabel__AnimationData:
+    .incbin "baserom_sa3.gba", 0x002DA4F0, 0x55BAC
+
+    .global gAnimations
+gAnimations: @ 0x0833009C
+    .incbin "baserom_sa3.gba", 0x0033009C, 1524 * 0x4
+
+    .global gSpriteTables
+gSpriteTables: @ 0x0833186C
+    mPtr gAnimations
+    mPtr gSpriteDimensions
+    mPtr gSpriteOamData
+    mPtr gSpritePalettes
+    mPtr gObjTiles_4bpp
+    mPtr gObjTiles_8bpp
+    mPtr gUnknown_08358528 
+@; NOTE: Ref. to gUnknown_08358528 is SA3-only
+
+@ OAM-Data referenced by gSpriteOamData
+    .global gUnknown_08331888
+gUnknown_08331888:
+    .incbin "baserom_sa3.gba", 0x00331888, 0x20FF0
+
+    .global gSpriteOamData
+gSpriteOamData: @ 0x08352878
+    .incbin "baserom_sa3.gba", 0x00352878, 1524 * 0x4
--- data/gfx_data_1.s
+++ data/gfx_data_1.s
@@ -5,42 +5,6 @@
 
 .section .rodata
 
-    .global gUnknown_082B9CD4
-gUnknown_082B9CD4:
-    .incbin "baserom_sa3.gba", 0x002B9CD4, 0x2045C
-
-    .global gUnknown_082DA130
-gUnknown_082DA130:
-    .incbin "baserom_sa3.gba", 0x002DA130, 0x55F6C
-
-    .global gAnimations
-gAnimations: @ 0x0833009C
-    .incbin "baserom_sa3.gba", 0x0033009C, 1524 * 0x4
-
-    .global gSpriteTables
-gSpriteTables: @ 0x0833186C
-    mPtr gAnimations
-    mPtr gSpriteDimensions
-    mPtr gSpriteOamData
-    mPtr gSpritePalettes
-    mPtr gObjTiles_4bpp
-    mPtr gObjTiles_8bpp
-    mPtr gUnknown_08358528 
-@; NOTE: Ref. to gUnknown_08358528 is SA3-only
-
-@ OAM-Data referenced by gSpriteOamData
-    .global gUnknown_08331888
-gUnknown_08331888:
-    .incbin "baserom_sa3.gba", 0x00331888, 0x20FF0
-
-    .global gSpriteOamData
-gSpriteOamData: @ 0x08352878
-    .incbin "baserom_sa3.gba", 0x00352878, 1524 * 0x4
-
-    .global gSpritePalettes
-gSpritePalettes:
-    .incbin "baserom_sa3.gba", 0x00354048, 0x44E0
-
     .global gUnknown_08358528
 gUnknown_08358528:
     .incbin "baserom_sa3.gba", 0x00358528, 0x28073C
@@ -83,7 +47,11 @@ gObjTiles_4bpp: @ 0x0876CE98
 
     .global gObjTiles_8bpp
 gObjTiles_8bpp:
-    .incbin "baserom_sa3.gba", 0x00DFE6F8, 0x2E02C
+    .incbin "baserom_sa3.gba", 0x00DFE6F8, 0x11100
+    
+    .global gTempLabel_SpriteDimensionsData
+gTempLabel_SpriteDimensionsData:
+    .incbin "baserom_sa3.gba", 0x00E0F7F8, 0x1CF2C
     
     .global gSpriteDimensions
 gSpriteDimensions:
