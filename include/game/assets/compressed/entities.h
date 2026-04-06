dbd8363e8bd0a1697055bea9d0f0a44c5e4b0bc5
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-04-06 17:05:28 +0200

    Fix build

---
+++ include/game/assets/compressed/entities.h
@@ -0,0 +1,182 @@
+#ifndef GUARD_DATA_ENTITIES_H
+#define GUARD_DATA_ENTITIES_H
+#include "global.h"
+
+extern const u8 sonic_factory_rings[];
+extern const u8 chao_playground_rings[];
+
+extern const u8 zone1_zone_map_rings[];
+
+extern const u8 zone1_act1_rings[];
+extern const u8 zone1_act1_enemies[];
+extern const u8 zone1_act1_itemboxes[];
+extern const u8 zone1_act1_interactables[];
+
+extern const u8 zone1_act2_rings[];
+extern const u8 zone1_act2_enemies[];
+extern const u8 zone1_act2_itemboxes[];
+extern const u8 zone1_act2_interactables[];
+
+extern const u8 zone1_act3_rings[];
+extern const u8 zone1_act3_enemies[];
+extern const u8 zone1_act3_itemboxes[];
+extern const u8 zone1_act3_interactables[];
+
+// Shared amongst all zones
+extern const u8 zone1_bonus_capsule_rings[];
+
+extern const u8 zone1_bonus_enemies_rings[];
+
+extern const u8 zone1_boss_rings[];
+extern const u8 zone1_boss_enemies[];
+extern const u8 zone1_boss_itemboxes[];
+extern const u8 zone1_boss_interactables[];
+
+extern const u8 zone2_zone_map_rings[];
+
+extern const u8 zone2_act1_rings[];
+extern const u8 zone2_act1_enemies[];
+extern const u8 zone2_act1_itemboxes[];
+extern const u8 zone2_act1_interactables[];
+
+extern const u8 zone2_act2_rings[];
+extern const u8 zone2_act2_enemies[];
+extern const u8 zone2_act2_itemboxes[];
+extern const u8 zone2_act2_interactables[];
+
+extern const u8 zone2_act3_rings[];
+extern const u8 zone2_act3_enemies[];
+extern const u8 zone2_act3_itemboxes[];
+extern const u8 zone2_act3_interactables[];
+
+extern const u8 zone2_bonus_enemies_rings[];
+
+extern const u8 zone2_boss_rings[];
+extern const u8 zone2_boss_enemies[];
+extern const u8 zone2_boss_itemboxes[];
+extern const u8 zone2_boss_interactables[];
+
+extern const u8 zone3_zone_map_rings[];
+
+extern const u8 zone3_act1_rings[];
+extern const u8 zone3_act1_enemies[];
+extern const u8 zone3_act1_itemboxes[];
+extern const u8 zone3_act1_interactables[];
+
+extern const u8 zone3_act2_rings[];
+extern const u8 zone3_act2_enemies[];
+extern const u8 zone3_act2_itemboxes[];
+extern const u8 zone3_act2_interactables[];
+
+extern const u8 zone3_act3_rings[];
+extern const u8 zone3_act3_enemies[];
+extern const u8 zone3_act3_itemboxes[];
+extern const u8 zone3_act3_interactables[];
+
+extern const u8 zone3_bonus_enemies_rings[];
+
+extern const u8 zone3_boss_rings[];
+extern const u8 zone3_boss_enemies[];
+extern const u8 zone3_boss_itemboxes[];
+extern const u8 zone3_boss_interactables[];
+
+extern const u8 zone4_zone_map_rings[];
+
+extern const u8 zone4_act1_rings[];
+extern const u8 zone4_act1_enemies[];
+extern const u8 zone4_act1_itemboxes[];
+extern const u8 zone4_act1_interactables[];
+
+extern const u8 zone4_act2_rings[];
+extern const u8 zone4_act2_enemies[];
+extern const u8 zone4_act2_itemboxes[];
+extern const u8 zone4_act2_interactables[];
+
+extern const u8 zone4_act3_rings[];
+extern const u8 zone4_act3_enemies[];
+extern const u8 zone4_act3_itemboxes[];
+extern const u8 zone4_act3_interactables[];
+
+extern const u8 zone4_bonus_enemies_rings[];
+
+extern const u8 zone4_boss_rings[];
+extern const u8 zone4_boss_enemies[];
+extern const u8 zone4_boss_itemboxes[];
+extern const u8 zone4_boss_interactables[];
+
+extern const u8 zone5_zone_map_rings[];
+
+extern const u8 zone5_act1_rings[];
+extern const u8 zone5_act1_enemies[];
+extern const u8 zone5_act1_itemboxes[];
+extern const u8 zone5_act1_interactables[];
+
+extern const u8 zone5_act2_rings[];
+extern const u8 zone5_act2_enemies[];
+extern const u8 zone5_act2_itemboxes[];
+extern const u8 zone5_act2_interactables[];
+
+extern const u8 zone5_act3_rings[];
+extern const u8 zone5_act3_enemies[];
+extern const u8 zone5_act3_itemboxes[];
+extern const u8 zone5_act3_interactables[];
+
+extern const u8 zone5_bonus_enemies_rings[];
+
+extern const u8 zone5_boss_rings[];
+extern const u8 zone5_boss_enemies[];
+extern const u8 zone5_boss_itemboxes[];
+extern const u8 zone5_boss_interactables[];
+
+extern const u8 zone6_zone_map_rings[];
+
+extern const u8 zone6_act1_rings[];
+extern const u8 zone6_act1_enemies[];
+extern const u8 zone6_act1_itemboxes[];
+extern const u8 zone6_act1_interactables[];
+
+extern const u8 zone6_act2_rings[];
+extern const u8 zone6_act2_enemies[];
+extern const u8 zone6_act2_itemboxes[];
+extern const u8 zone6_act2_interactables[];
+
+extern const u8 zone6_act3_rings[];
+extern const u8 zone6_act3_enemies[];
+extern const u8 zone6_act3_itemboxes[];
+extern const u8 zone6_act3_interactables[];
+
+extern const u8 zone6_bonus_enemies_rings[];
+
+extern const u8 zone6_boss_rings[];
+extern const u8 zone6_boss_enemies[];
+extern const u8 zone6_boss_itemboxes[];
+extern const u8 zone6_boss_interactables[];
+
+extern const u8 zone7_zone_map_rings[];
+
+extern const u8 zone7_act1_rings[];
+extern const u8 zone7_act1_enemies[];
+extern const u8 zone7_act1_itemboxes[];
+extern const u8 zone7_act1_interactables[];
+
+extern const u8 zone7_act2_rings[];
+extern const u8 zone7_act2_enemies[];
+extern const u8 zone7_act2_itemboxes[];
+extern const u8 zone7_act2_interactables[];
+
+extern const u8 zone7_act3_rings[];
+extern const u8 zone7_act3_enemies[];
+extern const u8 zone7_act3_itemboxes[];
+extern const u8 zone7_act3_interactables[];
+
+extern const u8 zone7_bonus_enemies_rings[];
+
+extern const u8 zone7_boss_rings[];
+extern const u8 zone7_boss_enemies[];
+extern const u8 zone7_boss_itemboxes[];
+extern const u8 zone7_boss_interactables[];
+
+extern const u8 altar_emerald_rings[];
+extern const u8 nonaggression_rings[];
+
+#endif // GUARD_DATA_ENTITIES_H
