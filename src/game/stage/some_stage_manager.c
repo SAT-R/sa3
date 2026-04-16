40754dc3c98882d8734ab4862a4bc7a357b33467
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-04-16 22:14:09 +0200

    Move Task_8065FAC into non-matching

--- src/game/stage/some_stage_manager.c
+++ src/game/stage/some_stage_manager.c
@@ -25,29 +25,25 @@ extern Strc_8E2EC98 *gUnknown_08E2EC98[];
 void Task_8065FAC();
 void sub_806619C(Task *);
 
-#if 0
-
-void Task_8065FAC(void) {
+// (92.76%) https://decomp.me/scratch/Wfgwn
+NONMATCH("asm/non_matching/game/stage/ssm__Task_8065FAC.inc", void Task_8065FAC(void))
+{
     u32 sp0;
     s32 sp4;
     s32 count;
-    s32 temp_r5_2;
     u16 *temp_r7;
     u32 temp_r0;
     u16 temp_r1;
     u16 var_r4;
-    u8 temp_r6;
     u8 var_r6;
     u8 var_r8;
     Strc_8E2EC98 *temp_r5;
+    u16 *max;
 
     SomeStageMgr *mgr = TASK_DATA(gCurTask);
     sp0 = mgr->unk1;
 
-    for(var_r8 = 0;
-        var_r8 < gUnknown_080D5248[sp0];
-        var_r8++)
-    {
+    for (var_r8 = 0; var_r8 < gUnknown_080D5248[sp0]; var_r8++) {
         temp_r5 = gUnknown_08E2EC98[sp0];
         temp_r0 = gUnknown_08E2EC98[sp0][var_r8].unk0;
         temp_r1 = temp_r5[var_r8].unk2;
@@ -55,32 +51,28 @@ void Task_8065FAC(void) {
         var_r6 = mgr->padd2[var_r8];
         var_r4 = var_r6 * (temp_r1 + 1);
         count = temp_r5[var_r8].ptr[var_r4];
-        if ((u32) (gStageData.timer - mgr->timers[var_r8]) > (count + 1))
-        {
+        if ((u32)(gStageData.timer - mgr->timers[var_r8]) > (count + 1)) {
             gFlags |= FLAGS_UPDATE_BACKGROUND_PALETTES;
             var_r4 += 1 + temp_r5[var_r8].unk2;
             if (var_r6 + 1 >= sp4) {
                 var_r4 = 0;
             }
             if (FLAGS_10000 & gFlags) {
-                sub_80C460C(&temp_r5[var_r8].ptr[var_r4 + 1], (u8) temp_r0, temp_r1);
+                sub_80C460C(&temp_r5[var_r8].ptr[var_r4 + 1], (u8)temp_r0, temp_r1);
             } else {
-                DmaSet(3, &temp_r5[var_r8].ptr[var_r4 + 1],
-                       &gBgPalette[temp_r0],
-                       0x80000000 | temp_r1);
+                DmaSet(3, &temp_r5[var_r8].ptr[var_r4 + 1], &gBgPalette[temp_r0], 0x80000000 | temp_r1);
                 gFlags |= FLAGS_UPDATE_BACKGROUND_PALETTES;
             }
             mgr->timers[var_r8] = gStageData.timer;
-            
-            if (++var_r6 >= (u32) sp4) {
+
+            if (++var_r6 >= (u32)sp4) {
                 var_r6 = 0;
             }
             mgr->padd2[var_r8] = var_r6;
         }
     }
 }
-
-#endif
+END_NONMATCH
 
 // This task gets created when entering any stage (except for Sonic Factory & Chao Garden)
 void sub_806611C(u8 arg0)
