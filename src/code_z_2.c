839e2d7aadd97e2beba062ebd49ee0202296a941
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-04-27 23:31:48 +0200

    More code_z_2.c progress

--- src/code_z_2.c
+++ src/code_z_2.c
@@ -13,7 +13,7 @@ s32 sub_80C6858(void);
 extern void GetInput(void);
 extern u8 gUnknown_03002C60;
 extern u8 gUnknown_0300620C;
-extern s8 gUnknown_03006C20;
+extern u8 gUnknown_03006C20;
 extern u16 gUnknown_082B533C[8];
 extern u16 gUnknown_082B5344[16 * PALETTE_LEN_4BPP];
 extern u8 gUnknown_082B5544[0x4000];
@@ -63,16 +63,28 @@ void sub_80C625C(void)
 typedef struct Strc3000428 {
     u8 unk0;
     u8 unk1;
-    s16 unk2;
+    u16 unk2;
     u16 unk4;
-    s16 unk6;
-    s16 unk8;
-    s16 unkA;
+    u16 unk6;
+    u16 unk8;
+    u16 unkA;
 } Strc3000428;
 extern Strc3000428 gUnknown_3000428;
 s32 gUnknown_3000434;
 
+extern void GetInput(void);
+extern u8 gUnknown_03002C60;
+extern u8 gUnknown_0300620C;
+extern u8 gUnknown_03006C20;
+extern u16 gUnknown_082B533C[8];
+extern u16 gUnknown_082B5344[16 * PALETTE_LEN_4BPP];
+extern u8 gUnknown_082B5544[0x4000];
+extern u8 gUnknown_082B9544[0x500];
+u8 *gUnknown_03002BF0;
+
+extern u16 gUnknown_082B533C[8];
 s32 gUnknown_3000434;
+u8 gUnknown_03006C20;
 extern u32 gUnknown_3000438;
 extern u32 gUnknown_300043C;
 
@@ -82,7 +94,6 @@ void sub_80C6318(void)
     s32 sp4;
     s32 sp8;
     s32 temp_r0_2;
-    s32 temp_r0_4;
     s32 temp_r6;
     u16 temp_r0_3;
     u16 temp_r2;
@@ -90,75 +101,70 @@ void sub_80C6318(void)
     u16 temp_r5;
     u16 var_r0;
     u32 temp_r0;
-    u32 temp_r6_2;
     u8 temp_r1;
-    u8 temp_r1_2;
+    s32 sioCntValue;
 
     gUnknown_3000438 = REG_SIODATA32;
     REG_TM3CNT_H = 0;
     REG_TM3CNT_L = 0x8000;
     switch (gUnknown_03006C20) {
         case 0:
-            temp_r6 = REG_SIOMULTI0;
+            temp_r6 = REG_SIODATA32;
             temp_r1 = gUnknown_3000428.unk0;
             temp_r0 = (u32)(temp_r6 << (temp_r1 * 0x10)) >> 0x10;
-            temp_r6_2 = (u32)(temp_r6 << ((1 - temp_r1) * 0x10)) >> 0x10;
-            temp_r5 = gUnknown_3000428.unkA;
-            if (temp_r5 == 0) {
+            temp_r6 = (u32)(temp_r6 << ((1 - temp_r1) * 0x10)) >> 0x10;
+
+            if (gUnknown_3000428.unkA == 0) {
                 temp_r2 = gUnknown_3000428.unk6;
                 if (temp_r0 == temp_r2) {
-                    temp_r3 = gUnknown_3000428.unk2;
-                    if (temp_r3 < 4) {
-                        if ((temp_r0 == (u16)~gUnknown_3000428.unk4) && (temp_r6_2 == (u16)~temp_r2)) {
-                            gUnknown_3000428.unk2 = (u16)(temp_r3 + 1);
+                    if (gUnknown_3000428.unk2 < 4) {
+                        if ((temp_r0 == (u16)~gUnknown_3000428.unk4) && (temp_r6 == (u16)~temp_r2)) {
+                            gUnknown_3000428.unk2++;
                         }
                     } else {
-                        gUnknown_3000428.unkA = (u16)temp_r6_2;
-                        if (temp_r6_2 == 0x8002) {
+                        gUnknown_3000428.unkA = temp_r6;
+                        if (temp_r6 == 0x8002) {
                             gUnknown_03006C20 = 1;
                             temp_r0_2 = sub_80C65F0(1U);
                             gUnknown_3000434 = temp_r0_2;
                             REG_SIODATA32 = temp_r0_2;
-                            gUnknown_3000428.unk2 = temp_r5;
-                            goto block_def;
+                            gUnknown_3000428.unk2 = 0;
+                            break;
                         } else {
-                            gUnknown_3000428.unkA = temp_r5;
+                            gUnknown_3000428.unkA = 0;
+                            gUnknown_3000428.unk2 = 0;
                         }
                     }
-                }
-
-                gUnknown_3000428.unk2 = temp_r5;
-                goto block_13;
-
-            } else {
-            block_13:
-                temp_r0_3 = gUnknown_3000428.unk2;
-                if ((u32)temp_r0_3 < 4) {
-                    var_r0 = gUnknown_082B533C[temp_r0_3];
                 } else {
-                    var_r0 = 0x8000;
+                    gUnknown_3000428.unk2 = 0;
                 }
-                gUnknown_3000428.unk4 = var_r0;
-                gUnknown_3000428.unk6 = (u16)~temp_r6_2;
-                temp_r1_2 = gUnknown_3000428.unk0;
-                REG_SIODATA32 = ((gUnknown_3000428.unk4 << ((1 - temp_r1_2) * 0x10)) + (gUnknown_3000428.unk6 << (temp_r1_2 * 0x10)));
+            }
+            {
+                u16 unk2 = gUnknown_3000428.unk2;
+                if (unk2 < 4) {
+                    gUnknown_3000428.unk4 = gUnknown_082B533C[unk2];
+                } else {
+                    gUnknown_3000428.unk4 = 0x8000;
+                }
+
+                gUnknown_3000428.unk6 = (u16)~temp_r6;
+                REG_SIODATA32 = ((gUnknown_3000428.unk4 << ((1 - gUnknown_3000428.unk0) * 0x10))
+                                 + (gUnknown_3000428.unk6 << (gUnknown_3000428.unk0 * 0x10)));
             }
 
-            REG_SIOCNT |= 0x80;
-            REG_TM3CNT_H = 0xC1;
             break;
         case 1:
             if (sub_80C6548(gUnknown_03006C20) != 0) {
                 gUnknown_3000428.unk2 = 0U;
-                CpuFill32(0, &gUnknown_3000428, 6);
+                CpuFill32(0, &gUnknown_3000428, 12);
                 gUnknown_03006C20 = 0;
             } else {
                 gUnknown_03006C20 = 2;
             }
-        block_28:
-            REG_SIOMULTI0 = gUnknown_3000434 = sub_80C65F0(gUnknown_03006C20);
-            REG_SIOCNT |= 0x80;
-            REG_TM3CNT_H = 0xC1;
+
+            gUnknown_3000434 = sub_80C65F0(gUnknown_03006C20);
+            ;
+            REG_SIODATA32 = gUnknown_3000434;
             break;
         case 2:
             if (sub_80C6548(gUnknown_03006C20) != 0) {
@@ -168,9 +174,10 @@ void sub_80C6318(void)
             } else {
                 gUnknown_03006C20 = 3;
             }
-            REG_SIOMULTI0 = gUnknown_3000434 = sub_80C65F0(gUnknown_03006C20);
-            REG_SIOCNT |= 0x80;
-            REG_TM3CNT_H = 0xC1;
+
+            gUnknown_3000434 = sub_80C65F0(gUnknown_03006C20);
+            ;
+            REG_SIODATA32 = gUnknown_3000434;
             break;
         case 3:
             if (sub_80C6548(gUnknown_03006C20) != 0) {
@@ -178,35 +185,43 @@ void sub_80C6318(void)
                 CpuFill32(0, &gUnknown_3000428, 12);
                 gUnknown_03006C20 = 0;
             }
-            REG_SIOMULTI0 = gUnknown_3000434 = sub_80C65F0(gUnknown_03006C20);
-            REG_SIOCNT |= 0x80;
-            REG_TM3CNT_H = 0xC1;
+
+            gUnknown_3000434 = sub_80C65F0(gUnknown_03006C20);
+            ;
+            REG_SIODATA32 = gUnknown_3000434;
             break;
+        case 4:
+        case 5:
         default:
         block_def:
             REG_IME = 0;
             REG_IE &= 0xFF7F;
             REG_IME = 1;
-            break;
+            return;
     }
+
+    REG_SIOCNT |= 0x80;
+    REG_TM3CNT_H = 0xC1;
 }
 #endif // inner
 
 s32 sub_80C6548(u8 arg0)
 {
     u32 temp_r5;
-    u8 temp_r4;
 
-    temp_r4 = arg0;
     temp_r5 = gUnknown_3000438 >> 0x1C;
     if (!sub_80C65B4()) {
-        switch (temp_r4) {
+        switch (arg0) {
             case 1:
-                gUnknown_300043C = ((gUnknown_3000438 * 0x10) >> 8) & temp_r4;
+                gUnknown_300043C = ((gUnknown_3000438 * 0x10) >> 8) & arg0;
                 if (temp_r5 == 1) {
                     break;
                 } else {
+                    return 1;
+                }
+                {
                 block_8:
+                default:
                     return 1;
                 }
             case 2:
@@ -215,12 +230,14 @@ s32 sub_80C6548(u8 arg0)
                         break;
                     }
                 }
-                goto block_8;
+                return 1;
             case 3:
                 if (temp_r5 != 3) {
-                    goto block_8;
+                    return 1;
                 }
         }
+    } else {
+        return 1;
     }
     return 0;
 }
@@ -228,22 +245,23 @@ s32 sub_80C6548(u8 arg0)
 bool8 sub_80C65B4(void)
 {
     u32 temp_r0;
-    u32 temp_r4;
+    s32 temp_r4;
     u8 var_r3;
     u32 temp_r0_2;
+    s32 var_r5;
     u8 var_r0;
 
     temp_r0 = gUnknown_3000438;
-    temp_r4 = temp_r0 >> 4;
-    var_r3 = temp_r4 >> 0x18;
-    var_r0 = 6;
-    do {
-        temp_r0_2 = var_r0 - 1;
-        var_r3 ^= (temp_r4 >> (temp_r0_2 * 4)) & 0xF;
-        var_r0 = temp_r0_2;
-    } while (var_r0 != 0);
+    temp_r4 = gUnknown_3000438 >> 4;
+    var_r5 = 0xF;
+    var_r5 &= temp_r0;
+    var_r3 = temp_r4 >> 24;
 
-    if (var_r3 == (0xF & temp_r0)) {
+    for (var_r0 = 6; var_r0 != 0; var_r0--) {
+        var_r3 ^= ((u32)temp_r4 >> ((var_r0 - 1) * 4)) & 0xF;
+    }
+
+    if (var_r3 == var_r5) {
         return 0;
     }
     return 1;
@@ -252,7 +270,8 @@ bool8 sub_80C65B4(void)
 s32 sub_80C65F0(u8 arg0)
 {
     u8 var_r2;
-    u8 var_r3;
+    u32 var_r0;
+    u32 var_r3;
     u32 var_r4;
 
 #ifndef BUG_FIX
@@ -274,16 +293,17 @@ s32 sub_80C65F0(u8 arg0)
             arg1 = (0xF & var_r3) | var_r4;
             break;
         case 2:
-            var_r4 = ((gUnknown_300043C & 0xFFFFFF) * 0x10) | 0x20000000;
-            var_r3 = (u8)(var_r4 >> 28);
+            var_r4 = ((gUnknown_300043C & 0xFFFFFF) << 4) | 0x20000000;
+            var_r3 = (var_r4 >> 28);
             for (var_r2 = 6; var_r2 != 0; var_r2--) {
                 var_r3 ^= (var_r4 >> (var_r2 * 4)) & 0xF;
             }
             arg1 = (0xF & var_r3) | var_r4;
             break;
         case 3:
-            var_r4 = (gUnknown_0300620C * 0x10) | 0x40000000;
-            var_r3 = (u8)(var_r4 >> 28);
+            var_r4 = (gUnknown_0300620C << 4) | 0x40000000;
+            var_r0 = var_r4 << 28;
+            var_r3 = var_r0 >> 1;
             var_r2 = 6;
             do {
                 var_r3 ^= (var_r4 >> (var_r2 * 4)) & 0xF;
