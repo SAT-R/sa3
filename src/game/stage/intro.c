ead1813b68c48127172d993dcbb0728b4167e716
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-05-25 21:57:48 +0200

    Match sub_8056AFC

--- src/game/stage/intro.c
+++ src/game/stage/intro.c
@@ -6,11 +6,17 @@
 #include "game/shared/stage/player.h"
 #include "game/save.h"
 #include "game/stage.h"
+#include "constants/animations.h"
 
-extern u8 gUnknown_080D1E18[6];
-void sub_80578EC(s32, s32);
-u32 sub_80C4C0C(u16 color);
-extern const TileInfo2 gUnknown_080D1D88[];
+typedef struct {
+    /* 0x00 */ u8 unk0;
+    /* 0x01 */ int_vcount unk1;
+    /* 0x02 */ int_vcount unk2;
+	/* 0x03 */ u8 unk3;
+	/* 0x04 */ u16 unk4;
+	/* 0x08 */ Background bg;
+    /* 0x48 */ Sprite s;
+} StageIntro_70; /* 0x70 */
 
 typedef struct {
     /* 0x000 */ u8 unk0;
@@ -35,8 +41,37 @@ typedef struct {
     /* 0x16C */ void *unk16C;
 } Strc_170_StageIntro;
 
+void sub_8057054(void);
 void Task_8057FE8(void);
 void TaskDestructor_StageIntro(struct Task *t);
+void sub_80578EC(s32, s32);
+u32 sub_80C4C0C(u16 color);
+
+extern u8 gUnknown_080D1E18[6];
+extern const TileInfo2 gUnknown_080D1D88[];
+
+void sub_8056AFC(u8 param0)
+{
+	struct Task *t = TaskCreate(sub_8057054, sizeof(StageIntro_70), 0x84 << 6, 0, NULL);
+    StageIntro_70 *strc70 = TASK_DATA(t);
+
+	strc70->unk0 = param0;
+	strc70->unk4 = 0;
+	strc70->unk1 = 0;
+	strc70->unk2 = DISPLAY_HEIGHT;
+	strc70->unk3 = 16;
+
+	gDispCnt |= 0x4000;
+	gDispCnt &= ~0x2000;
+
+	gWinRegs[1] = WIN_RANGE(0, DISPLAY_WIDTH);
+	gWinRegs[3] = WIN_RANGE(0, DISPLAY_HEIGHT);
+	gWinRegs[4] = 0x3F00;
+	gWinRegs[5] = 0;
+	gBldRegs.bldCnt   = 0xBF;
+	gBldRegs.bldAlpha = 0;
+	gBldRegs.bldY     = 0x10;
+}
 
 void CreateStageIntro(void)
 {
@@ -290,3 +325,83 @@ void CreateStageIntro(void)
         strc->unk4 = 5;
     }
 }
+
+#if 0
+void sub_8057054(StageIntro_70 *strc70)
+{
+    s32 sp0;
+    TileInfo2 *temp_r4_3;
+    s32 temp_r4_2;
+    s32 var_r1;
+    u8 temp_r1;
+    u8 temp_r4;
+
+    temp_r1 = strc70->unk1 + 8;
+    strc70->unk1 = temp_r1;
+    strc70->unk2 -= 8;
+    if ((u32)temp_r1 <= 0x43U) {
+        gWinRegs[3] = strc70->unk2 | (strc70->unk1 << 8);
+        return;
+    }
+    strc70->unk1 = 0x44;
+    strc70->unk2 = 0x5C;
+    gWinRegs[3] = 0x4400 | 0x5C;
+    temp_r4 = gStageData.zone;
+    strc70->bg.graphics.dest = (void *)0x06008000;
+    strc70->bg.graphics.anim = 0;
+    strc70->bg.layoutVram = (u16 *)0x0600A000;
+    strc70->bg.unk18 = 0;
+    strc70->bg.unk1A = 0;
+    strc70->bg.tilemapId = 0x162;
+    strc70->bg.unk1E = 0;
+    strc70->bg.unk20 = 0;
+    strc70->bg.unk22 = 0;
+    strc70->bg.unk24 = 0;
+    strc70->bg.targetTilesX = 0x20;
+    strc70->bg.targetTilesY = 0x15;
+    strc70->bg.paletteOffset = 0;
+    strc70->bg.animFrameCounter = 0;
+    strc70->bg.animDelayCounter = 0;
+    strc70->bg.flags = 0x10;
+    strc70->bg.scrollX = 0;
+    strc70->bg.scrollY = 0;
+    sp0 = 0;
+    DrawBackground(&strc70->bg);
+    gDispCnt |= 0x100;
+    *gBgCntRegs = 0x1408;
+    gBgScrollRegs[0][0] = 0;
+    gBgScrollRegs[0][1] = (temp_r4 * 0x18) - 0x44;
+    (void *)0x040000D4->unk0 = (void *)(&Palette_unknown_354 + 0xE0);
+    (void *)0x040000D4->unk4 = &gBgPalette[0x70];
+    (void *)0x040000D4->unk8 = 0x80000090;
+    gFlags |= 1;
+    var_r1 = 0;
+    if (gLoadedSaveGame.unk366 != 0) {
+        var_r1 = 9;
+    }
+    temp_r4_2 = temp_r4 + var_r1;
+    sp0 = 0;
+    strc70->s.tiles = VramMalloc(*((temp_r4_2 * 8) + &gUnknown_080D1D88->numTiles));
+    temp_r4_3 = &gUnknown_080D1D88[temp_r4_2];
+    strc70->s.anim = temp_r4_3->anim;
+    strc70->s.variant = temp_r4_3->variant;
+    strc70->s.oamFlags = 0x480;
+    strc70->s.animCursor = 0;
+    strc70->s.qAnimDelay = 0;
+    strc70->s.prevVariant = 0xFF;
+    strc70->s.animSpeed = 0x10;
+    strc70->s.palId = 0;
+    strc70->s.frameFlags = 0;
+    strc70->s.x = 0xA8;
+    strc70->s.y = 0x50;
+    strc70->s.hitboxes[0].index = -1;
+    sp0 = 0;
+    UpdateSpriteAnimation(&strc70->s);
+    strc70->unk4 = 0x3C;
+    gWinRegs[4] = 0x3F00;
+    gBldRegs.bldCnt = 0xAE;
+    gBldRegs.bldAlpha = 0;
+    gBldRegs.bldY = 0x10;
+    gCurTask->main = sub_8057F7C;
+}
+#endif
