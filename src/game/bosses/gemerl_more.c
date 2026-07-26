9f51c6c634f6ddaec0e4a6ee72dc262e94404400
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-07-26 20:41:47 +0200

    Match sub_8079758

--- src/game/bosses/gemerl_more.c
+++ src/game/bosses/gemerl_more.c
@@ -1,5 +1,6 @@
 #include "global.h"
 #include "core.h"
+#include "trig.h"
 #include "malloc_vram.h"
 #include "constants/animations.h"
 
@@ -127,3 +128,69 @@ END_NONMATCH
 // Very similar to sub_8079194
 NONMATCH("asm/non_matching/game/bosses/gmore__sub_8079468.inc", void sub_8079468(u8 *arg0, u8 arg1, u8 arg2, u8 *arg3)) { }
 END_NONMATCH
+
+typedef struct {
+    /* 0x00 */ s32 unk0;
+    /* 0x04 */ s32 unk4;
+    /* 0x08 */ s16 unk8;
+    /* 0x0A */ s16 unkA;
+    /* 0x0C */ s16 unkC;
+    /* 0x0E */ u8 unkE;
+    /* 0x0F */ bool8 vramUsesHeap;
+    /* 0x10 */ Sprite s;
+} GemerlMore_38;
+
+typedef struct {
+    /* 0x00 */ AnimId anim;
+    /* 0x02 */ u8 pattern;
+    /* 0x04 */ u32 numTiles;
+    /* 0x08 */ u32 vramOffset;
+} Strc_80D5A6C;
+
+extern const Strc_80D5A6C gUnknown_080D5A6C[10];
+void Task_38_807989C(void);
+void TaskDestructor_38_807A4E4(struct Task *t);
+
+struct Task *sub_8079758(u8 stateIndex, s16 x, s16 y, s16 arg3, u16 angle, u8 arg5, s16 arg6, u8 *vram)
+{
+    Task *spC;
+    Task *temp_r0_2;
+    s16 temp_r4;
+    s32 temp_r1_2;
+    s32 var_r4;
+    u8 *vramTarget;
+    void *temp_r0_4;
+    struct Task *t = TaskCreate(Task_38_807989C, sizeof(GemerlMore_38), 0x2100U, 0U, TaskDestructor_38_807A4E4);
+    GemerlMore_38 *strc38 = TASK_DATA(t);
+    Sprite *s = &strc38->s;
+    strc38->unkE = arg5;
+    strc38->unk8 = ((COS(angle % SIN_PERIOD) * arg3) >> 14);
+    strc38->unkA = ((SIN(angle % SIN_PERIOD) * arg3) >> 14);
+    strc38->unkC = arg6;
+
+    if (vram != NULL) {
+        strc38->vramUsesHeap = FALSE;
+        vramTarget = (vram + gUnknown_080D5A6C[stateIndex].vramOffset);
+    } else {
+        strc38->vramUsesHeap = TRUE;
+        vramTarget = VramMalloc(gUnknown_080D5A6C[stateIndex].numTiles);
+    }
+    s->tiles = vramTarget;
+    s->oamFlags = 0x200;
+    s->anim = gUnknown_080D5A6C[stateIndex].anim;
+    s->variant = gUnknown_080D5A6C[stateIndex].pattern;
+    s->qAnimDelay = 0;
+    s->prevVariant = 0xFF;
+    s->animSpeed = 0x10;
+    s->palId = 0;
+    s->x = 0;
+    s->y = 0;
+    s->frameFlags = 0x1000;
+
+    strc38->unk0 = Q(x);
+    strc38->unk4 = Q(y);
+
+	return t;
+}
+
+
