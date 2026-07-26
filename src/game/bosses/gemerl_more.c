17761894519dd85f0614ca2dbc03cbad126cb80c
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-07-26 17:07:22 +0200

    Move GemerlUnusedRecursiveFunc into non-matching

--- src/game/bosses/gemerl_more.c
+++ src/game/bosses/gemerl_more.c
@@ -1,6 +1,7 @@
 #include "global.h"
 #include "core.h"
 #include "malloc_vram.h"
+#include "constants/animations.h"
 
 typedef struct {
     /* 0x00 */ Sprite s;
@@ -11,18 +12,19 @@ typedef struct {
 typedef struct {
     /* 0x00 */ u16 unk0;
     /* 0x04 */ s32 unk4[3][2];
-    /* 0x1C */ Sprite spr1C;
+    /* 0x1C */ Sprite sprEggman;
     /* 0x44 */ u8 filler44[0x10];
-    /* 0x54 */ Sprite spr54;
+    /* 0x54 */ Sprite sprEggMobile;
     /* 0x7C */ u8 filler7C[0x10];
-    /* 0x8C */ Sprite spr8C;
+    /* 0x8C */ Sprite sprGemerl;
     /* 0xB4 */ u8 fillerB4[0x10];
     /* 0xC4 */ VoidFn func;
 } GemerlMore_C8;
 
-void Task_2C_807A514(void);
+void sub_8078E34(s32 *param0, VoidFn fn);
 void Task_C8_807990C(void);
 void TaskDestructor_C8_807A500(struct Task *t);
+void Task_2C_807A514(void);
 
 void sub_8078DB0(s16 anim, s8 pattern, s16 param2, bool8 param3)
 {
@@ -34,8 +36,8 @@ void sub_8078DB0(s16 anim, s8 pattern, s16 param2, bool8 param3)
 
     s->tiles = 0;
     s->oamFlags = 0;
-    s->anim = (u16)anim;
-    s->variant = (u8)pattern;
+    s->anim = anim;
+    s->variant = pattern;
     s->qAnimDelay = 0;
     s->prevVariant = -1;
     s->animSpeed = SPRITE_ANIM_SPEED(10. / 16.);
@@ -68,11 +70,12 @@ void sub_8078E34(s32 *param0, VoidFn fn)
         }
     }
 
-    vram = VramMalloc(0x58U);
-    s = &strcC8->spr1C;
+    vram = VramMalloc(88);
+
+    s = &strcC8->sprEggman;
     s->tiles = vram;
     s->oamFlags = 0x4C0;
-    s->anim = 0x4F8;
+    s->anim = ANIM_EGGMAN_SIDE;
     s->variant = 4;
     s->qAnimDelay = 0;
     s->prevVariant = 0xFF;
@@ -82,10 +85,11 @@ void sub_8078E34(s32 *param0, VoidFn fn)
     s->y = param0[1];
     s->frameFlags = 0x2000;
     vram += 16 * TILE_SIZE_4BPP;
-    s2 = &strcC8->spr54;
+
+    s2 = &strcC8->sprEggMobile;
     s2->tiles = vram;
     s2->oamFlags = 0x500;
-    s2->anim = 0x4F7;
+    s2->anim = ANIM_NEO_EGGMOBILE;
     s2->variant = 0;
     s2->qAnimDelay = 0;
     s2->prevVariant = -1;
@@ -95,10 +99,11 @@ void sub_8078E34(s32 *param0, VoidFn fn)
     s2->y = param0[1];
     s2->frameFlags = 0x2000;
     vram += 36 * TILE_SIZE_4BPP;
-    s = &strcC8->spr8C;
+
+    s = &strcC8->sprGemerl;
     s->tiles = vram;
     s->oamFlags = 0x480;
-    s->anim = 0x506;
+    s->anim = ANIM_GEMERL_IDLE;
     s->variant = 0;
     s->qAnimDelay = 0;
     s->prevVariant = -1;
@@ -108,3 +113,9 @@ void sub_8078E34(s32 *param0, VoidFn fn)
     s->y = param0[1];
     s->frameFlags = 0x2000;
 }
+
+// TODO: This *might* be an inline function?
+//       It is unused, but there's similar structure below it.
+//       But usually an implementation of an inlined function appears later in OG code
+NONMATCH("asm/non_matching/game/bosses/gmore__GemerlUnusedRecursiveFunc.inc", void GemerlUnusedRecursiveFunc()) { }
+END_NONMATCH
