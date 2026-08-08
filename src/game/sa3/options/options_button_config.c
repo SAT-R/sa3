1e85597c369ac9fe9e088fecbd9247c405036025
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-08-08 22:11:33 +0200

    Progress on options_button_config.c

--- src/game/sa3/options/options_button_config.c
+++ src/game/sa3/options/options_button_config.c
@@ -1,5 +1,7 @@
 #include "global.h"
 #include "core.h"
+#include "game/player_controls.h"
+#include "game/save.h"
 
 typedef struct {
     u8 unk0;
@@ -28,7 +30,7 @@ typedef struct {
     Sprite spr224;
 } OptionsButtonConfig; /* 0x24C */
 
-void Options_ButtonConfig(s16 highlitButton, u8 *vramBase, s16 *arg2, u8 *arg3, OptionsButtonConfig *config);
+void Options_ButtonConfig(s16 highlitButton, u8 *vramBase, s16 *arg2, u8 *arg3);
 
 void sub_8091150(OptionsButtonConfig *config);
 void Task_Options_ButtonConfig_Init(void);
@@ -46,39 +48,20 @@ extern const TileInfo2 gUnknown_080D7234[6];
 extern const TileInfo2 gUnknown_080D7264[23];
 extern const TileInfo2 gUnknown_080D731C[3];
 extern const TileInfo2 gUnknown_080D7334[6];
+extern const TileInfo2 gUnknown_080D736C[2];
 extern const TileInfo2 gUnknown_080D7364;
+extern const ButtonConfigPacked gUnknown_080D73BC;
 
-#if 0
-void Options_ButtonConfig(s16 highlitButton, u8 *vramBase, s16 *arg2, u8 *arg3) {
-    u16 sp4;
-    u16 spC;
-    u16 sp10;
-    u8 *sp14;
-    Vec2_32 *sp18;
-    void *sp1C;
-    u16 *sp20;
-    u16 *sp24;
-    Vec2_32 *sp28;
-    s32 *sp2C;
-    Vec2_32 *temp_r2;
-    Vec2_32 *temp_r4;
-    s32 *temp_r3;
-    s32 temp_r2_2;
-    u16 *temp_r2_3;
-    u16 temp_r1;
+// (99.44%) https://decomp.me/scratch/tWAor
+NONMATCH("asm/non_matching/game/sa3/options/opt__Options_ButtonConfig.inc", void Options_ButtonConfig(s16 highlitButton, u8 *vramBase, s16 *arg2, u8 *arg3))
+{
+    u16 sp4[3] = { LOADED_SAVE->buttonConfig.jump, LOADED_SAVE->buttonConfig.attack, LOADED_SAVE->buttonConfig.trick };
+    u8 spC[3] = { 0, 1, 2 };
     u8 var_r6;
-    u8 var_r6_2;
-    struct Task *t;
-    OptionsButtonConfig *config;
+    struct Task *t
+        = TaskCreate(Task_Options_ButtonConfig_Init, sizeof(OptionsButtonConfig), 0x100U, 0U, TaskDestructor_Options_ButtonConfig);
+    OptionsButtonConfig *config = TASK_DATA(t);
 
-    sp14 = vramBase;
-    spC = gLoadedSaveGame.buttonConfig.jump;
-    subroutine_arg0.unkE = (u16) gLoadedSaveGame.buttonConfig.attack;
-    sp10 = gLoadedSaveGame.buttonConfig.trick;
-    memcpy(&sp4, &spC, 6);
-    memcpy(&spC, &gUnknown_080D73BC, 3);
-    t = TaskCreate((void (*)()) Task_Options_ButtonConfig_Init, sizeof(OptionsButtonConfig), 0x100U, 0U, TaskDestructor_Options_ButtonConfig);
-    config = TASK_DATA(t);
     config->highlitButton = highlitButton;
     config->initArg3 = arg3;
     config->unk20 = 0;
@@ -87,200 +70,188 @@ void Options_ButtonConfig(s16 highlitButton, u8 *vramBase, s16 *arg2, u8 *arg3) 
     config->unk2 = 0;
     config->unk22 = 0;
     config->unk4 = 1;
-    var_r6 = 0;
-    do {
-        temp_r1 = ((var_r6 * 2) + sp)->unk4;
-        switch (temp_r1) {                          /* irregular */
-        case 0x1:
-            config->unkC[2][0] = (u16) *(&spC + var_r6);
-            break;
-        case 0x2:
-            config->unkC[2][1] = (u16) *(&spC + var_r6);
-            break;
-        case 0x100:
-            config->unkC[2][2] = (u16) *(&spC + var_r6);
-            break;
+
+    for (var_r6 = 0; var_r6 < ARRAY_COUNT(sp4); var_r6++) {
+        switch (sp4[var_r6]) {
+            case A_BUTTON:
+                config->unkC[2][0] = spC[var_r6];
+                break;
+
+            case B_BUTTON:
+                config->unkC[2][1] = spC[var_r6];
+                break;
+
+            case R_BUTTON:
+                config->unkC[2][2] = spC[var_r6];
+                break;
         }
-        var_r6 += 1;
-    } while ((u32) var_r6 <= 2U);
+    }
+
     config->unk20 = config->unkC[2][0];
-    var_r6_2 = 0;
-    temp_r2 = config->unk24[0];
-    sp28 = temp_r2;
-    sp2C = &config->unk24[0][0].y;
-    sp18 = config->unk24[1];
-    sp1C = temp_r2 + 0x1C;
-    sp20 = &config->unk64;
-    sp24 = &config->unk66;
-    do {
-        temp_r2_2 = var_r6_2 * 8;
-        temp_r4 = &sp28[var_r6_2];
-        temp_r4->x = 0x70 - gBgScrollRegs[1][0];
-        temp_r3 = sp2C + temp_r2_2;
-        *temp_r3 = 0x2D - gBgScrollRegs[1][1];
-        sp18[var_r6_2].x = temp_r4->x - 0x46;
-        *(sp1C + temp_r2_2) = *temp_r3;
-        temp_r2_3 = &config->unkC[2][var_r6_2];
-        config->unkC[0][var_r6_2] = *temp_r2_3;
-        config->unkC[1][var_r6_2] = *temp_r2_3;
-        var_r6_2 += 1;
-    } while ((u32) var_r6_2 <= 2U);
-    config->unk54 = 0 - gBgScrollRegs[1][0];
-    config->unk58 = 0 - gBgScrollRegs[1][1];
-    *sp20 = (0 - (u16) gBgScrollRegs[1][0]) + 0x64;
-    *sp24 = 0 - (u16) gBgScrollRegs[1][1];
-    config->unk5C = 0 - gBgScrollRegs[1][0];
-    config->unk60 = 0 - gBgScrollRegs[1][1];
-    config->vram68 = sp14;
-    config->unk0 = gLoadedSaveGame.language;
+
+    for (var_r6 = 0; var_r6 < ARRAY_COUNT(config->unk24[0]); var_r6++) {
+        u8 argh[8];
+
+        config->unk24[0][var_r6].x = 0x70 - gBgScrollRegs[1][0];
+        config->unk24[0][var_r6].y = 0x2D - gBgScrollRegs[1][1];
+        config->unk24[1][var_r6].x = config->unk24[0][var_r6].x - 0x46;
+        config->unk24[1][var_r6].y = config->unk24[0][var_r6].y;
+        config->unkC[0][var_r6] = config->unkC[2][var_r6];
+        config->unkC[1][var_r6] = config->unkC[2][var_r6];
+    }
+
+    config->unk54 = -gBgScrollRegs[1][0];
+    config->unk58 = -gBgScrollRegs[1][1];
+    config->unk64 = -gBgScrollRegs[1][0] + 100;
+    config->unk66 = -gBgScrollRegs[1][1];
+    config->unk5C = -gBgScrollRegs[1][0];
+    config->unk60 = -gBgScrollRegs[1][1];
+    config->vram68 = vramBase;
+    config->unk0 = LOADED_SAVE->language;
     sub_8091150(config);
 }
+END_NONMATCH
 
 void sub_8091150(OptionsButtonConfig *config) {
     Vec2_32 *sp0;
     s32 *sp4;
-    TileInfo2 *sp8;
     Sprite *temp_r0;
-    Sprite *temp_r0_2;
-    Sprite *temp_r2_2;
     Sprite *temp_r4;
-    TileInfo2 *temp_r2;
     TileInfo2 *var_r0;
     TileInfo2 *var_r3_2;
     s32 temp_r3;
-    s32 temp_r5;
     u16 var_r3;
     u8 var_r7;
-    u8 var_r7_2;
-    u8 var_r7_3;
     void *temp_r3_2;
 
-    config->spr224.tiles = config->vram68;
-    config->vram68 += *((config->unk0 * 8) + &gUnknown_080D7334->numTiles) << 5;
-    config->spr224.anim = gUnknown_080D7334[config->unk0].anim;
-    config->spr224.variant = gUnknown_080D7334[config->unk0].variant;
-    config->spr224.prevVariant = 0xFF;
-    config->spr224.x = (s16) ((u16) config->unk64 >> 8);
-    config->spr224.y = (s16) ((u16) config->unk66 >> 8);
-    config->spr224.oamFlags = 0;
-    config->spr224.animCursor = 0;
-    config->spr224.qAnimDelay = 0;
-    config->spr224.animSpeed = 0x10;
-    config->spr224.palId = 0;
-    config->spr224.frameFlags = 0;
-    config->spr224.hitboxes[0].index = -1;
-    UpdateSpriteAnimation(&config->spr224);
-    var_r7 = 0;
-    var_r3 = 0;
-    sp0 = config->unk24[0];
-    sp4 = &config->unk24[0][0].y;
-    do {
-        temp_r0 = &config->spr6C[var_r7];
-        temp_r0->tiles = config->vram68;
-        config->vram68 += *(((var_r7 + (config->unk0 * 4)) * 8) + &gUnknown_080D7264->numTiles) << 5;
-        temp_r0->anim = gUnknown_080D7264[var_r7 + (config->unk0 * 4)].anim;
-        temp_r0->variant = gUnknown_080D7264[var_r7 + (config->unk0 * 4)].variant;
-        temp_r0->prevVariant |= 0xFF;
-        temp_r5 = var_r7 * 8;
-        temp_r0->x = (s16) ((s32) config->unk24[0][var_r7].x >> 8);
-        temp_r0->y = (s16) ((s32) *(&config->unk24[0][0].y + temp_r5) >> 8);
-        temp_r0->oamFlags = 0x40;
-        temp_r0->animCursor = var_r3;
-        temp_r0->qAnimDelay = (s16) var_r3;
-        temp_r0->animSpeed = 0x10;
-        temp_r0->palId = 0;
-        temp_r0->frameFlags = (u32) var_r3;
-        temp_r0->hitboxes[0].index = -1;
-        sp8 = (TileInfo2 *) var_r3;
-        UpdateSpriteAnimation(temp_r0);
-        temp_r4 = &config->sprE4[var_r7];
-        temp_r4->tiles = config->vram68;
-        config->vram68 += *(temp_r5 + &gUnknown_080D731C->numTiles) << 5;
-        temp_r2 = &gUnknown_080D731C[var_r7];
-        temp_r4->anim = temp_r2->anim;
-        temp_r4->variant = temp_r2->variant;
-        temp_r4->prevVariant |= 0xFF;
-        temp_r4->x = (s16) ((s32) config->unk24[1][var_r7].x >> 8);
-        temp_r4->y = (s16) ((s32) *(&config->unk24[1][0].y + temp_r5) >> 8);
-        temp_r4->oamFlags = 0x40;
-        temp_r4->animCursor = var_r3;
-        temp_r4->qAnimDelay = (s16) var_r3;
-        temp_r4->animSpeed = 0x10;
-        temp_r4->palId = 0;
-        temp_r4->frameFlags = (u32) var_r3;
-        temp_r4->hitboxes[0].index = -1;
-        sp8 = (TileInfo2 *) var_r3;
-        UpdateSpriteAnimation(temp_r4);
-        var_r7 += 1;
-    } while ((u32) var_r7 <= 2U);
-    var_r7_2 = 0;
-    do {
-        temp_r0_2 = &config->spr1AC[var_r7_2];
-        temp_r0_2->tiles = config->vram68;
-        temp_r3 = var_r7_2 * 8;
-        config->vram68 += *(temp_r3 + (&gUnknown_080D736C + 4)) << 5;
-        temp_r3_2 = temp_r3 + &gUnknown_080D736C;
-        temp_r0_2->anim = temp_r3_2->unk0;
-        temp_r0_2->variant = temp_r3_2->unk2;
-        temp_r0_2->prevVariant = 0xFF;
-        temp_r0_2->x = sp0[config->highlitButton].x - 8;
-        temp_r0_2->y = (*(sp4 + (config->highlitButton * 8)) + ((u16) config->highlitButton * 0x10)) - 4;
-        temp_r0_2->oamFlags = 0;
-        temp_r0_2->animCursor = 0;
-        temp_r0_2->qAnimDelay = 0;
-        temp_r0_2->animSpeed = 0x10;
-        temp_r0_2->palId = 0;
-        temp_r0_2->frameFlags = 0;
-        temp_r0_2->hitboxes[0].index = -1;
-        UpdateSpriteAnimation(temp_r0_2);
-        var_r7_2 += 1;
-    } while ((u32) var_r7_2 <= 1U);
-    config->spr1FC.tiles = config->vram68;
-    config->vram68 += gUnknown_080D7364.numTiles << 5;
-    config->spr1FC.anim = gUnknown_080D7364.anim;
-    config->spr1FC.variant = gUnknown_080D7364.variant;
-    config->spr1FC.prevVariant |= ~0;
-    config->spr1FC.x = sp0[config->highlitButton].x - 8;
-    config->spr1FC.y = (*(sp4 + (config->highlitButton * 8)) + ((u16) config->highlitButton * 0x10)) - 4;
-    config->spr1FC.oamFlags = 0;
-    config->spr1FC.animCursor = 0;
-    config->spr1FC.qAnimDelay = 0;
-    config->spr1FC.animSpeed = 0x10;
-    config->spr1FC.palId = 0;
-    config->spr1FC.frameFlags = 0;
-    config->spr1FC.hitboxes[0].index = -1;
-    UpdateSpriteAnimation(&config->spr1FC);
-    var_r7_3 = 0;
-    var_r3_2 = gUnknown_080D7024;
-    do {
-        temp_r2_2 = &config->spr15C[var_r7_3];
-        temp_r2_2->tiles = config->vram68;
-        if (var_r7_3 == 0) {
-            config->vram68 += *((config->unk0 * 8) + &var_r3_2->numTiles) << 5;
-            temp_r2_2->anim = var_r3_2[config->unk0].anim;
-            var_r0 = &var_r3_2[config->unk0];
-        } else {
-            config->vram68 += *((config->unk0 * 8) + &gUnknown_080D7234->numTiles) << 5;
-            temp_r2_2->anim = gUnknown_080D7234[config->unk0].anim;
-            var_r0 = &gUnknown_080D7234[config->unk0];
+    {
+        Sprite *s = &config->spr224;
+        s->tiles = config->vram68;
+        config->vram68 += gUnknown_080D7334[config->unk0].numTiles * TILE_SIZE_4BPP;
+        s->anim = gUnknown_080D7334[config->unk0].anim;
+        s->variant = gUnknown_080D7334[config->unk0].variant;
+        s->prevVariant = 0xFF;
+        s->x = (s16) ((u16) config->unk64 >> 8);
+        s->y = (s16) ((u16) config->unk66 >> 8);
+        s->oamFlags = 0;
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->animSpeed = 0x10;
+        s->palId = 0;
+        s->frameFlags = 0;
+        s->hitboxes[0].index = -1;
+        UpdateSpriteAnimation(s);
+    }
+
+    for(var_r7 = 0; var_r7 < 3; var_r7++)
+    {
+        {
+            Sprite *s = &config->spr6C[var_r7];
+            s->tiles = config->vram68;
+            config->vram68 += gUnknown_080D7264[var_r7 + (config->unk0 * 4)].numTiles * TILE_SIZE_4BPP;
+            s->anim = gUnknown_080D7264[var_r7 + (config->unk0 * 4)].anim;
+            s->variant = gUnknown_080D7264[var_r7 + (config->unk0 * 4)].variant;
+            s->prevVariant = -1;
+            s->x = (config->unk24[0][var_r7].x >> 8);
+            s->y = (config->unk24[0][var_r7].y >> 8);
+            s->oamFlags = 0x40;
+            s->animCursor = 0;
+            s->qAnimDelay = 0;
+            s->animSpeed = 0x10;
+            s->palId = 0;
+            s->frameFlags = 0;
+            s->hitboxes[0].index = -1;
+            UpdateSpriteAnimation(s);
         }
-        temp_r2_2->variant = var_r0->variant;
-        temp_r2_2->prevVariant = 0xFF;
-        temp_r2_2->x = (s16) ((s32) config->unk54 >> 8);
-        temp_r2_2->y = (s16) ((s32) config->unk58 >> 8);
-        temp_r2_2->oamFlags = 0;
-        temp_r2_2->animCursor = 0;
-        temp_r2_2->qAnimDelay = 0;
-        temp_r2_2->animSpeed = 0x10;
-        temp_r2_2->palId = 0;
-        temp_r2_2->frameFlags = 0;
-        temp_r2_2->hitboxes[0].index = -1;
-        sp8 = var_r3_2;
-        UpdateSpriteAnimation(temp_r2_2);
-        var_r7_3 += 1;
-    } while ((u32) var_r7_3 <= 1U);
+
+        {
+            Sprite *s = &config->sprE4[var_r7];
+            s->tiles = config->vram68;
+            config->vram68 += gUnknown_080D731C[var_r7].numTiles * TILE_SIZE_4BPP;
+            s->anim = gUnknown_080D731C[var_r7].anim;
+            s->variant = gUnknown_080D731C[var_r7].variant;
+            s->prevVariant = -1;
+            s->x = (config->unk24[1][var_r7].x >> 8);
+            s->y = (config->unk24[1][var_r7].y >> 8);
+            s->oamFlags = 0x40;
+            s->animCursor = 0;
+            s->qAnimDelay = 0;
+            s->animSpeed = 0x10;
+            s->palId = 0;
+            s->frameFlags = 0;
+            s->hitboxes[0].index = -1;
+            UpdateSpriteAnimation(s);
+        }
+    }
+
+    for(var_r7 = 0; var_r7 < 2; var_r7++)
+    {
+        Sprite *s = &config->spr1AC[var_r7];
+        s->tiles = config->vram68;
+        temp_r3 = var_r7 * 8;
+        config->vram68 += gUnknown_080D736C[var_r7].numTiles * TILE_SIZE_4BPP;
+        s->anim = gUnknown_080D736C[var_r7].anim;
+        s->variant = gUnknown_080D736C[var_r7].variant;
+        s->prevVariant = -1;
+        s->x = config->unk24[0][config->highlitButton].x - 8;
+        s->y = config->unk24[0][config->highlitButton].y + ((u16) config->highlitButton * 0x10) - 4;
+        s->oamFlags = 0;
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->animSpeed = 0x10;
+        s->palId = 0;
+        s->frameFlags = 0;
+        s->hitboxes[0].index = -1;
+        UpdateSpriteAnimation(s);
+    }
+
+    {
+        Sprite *s = &config->spr1FC;
+        s->tiles = config->vram68;
+        config->vram68 += gUnknown_080D7364.numTiles << 5;
+        s->anim = gUnknown_080D7364.anim;
+        s->variant = gUnknown_080D7364.variant;
+        s->prevVariant |= ~0;
+        s->x = config->unk24[0][config->highlitButton].x - 8;
+        s->y = config->unk24[0][config->highlitButton].y + ((u16) config->highlitButton * 0x10) - 4;
+        s->oamFlags = 0;
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->animSpeed = 0x10;
+        s->palId = 0;
+        s->frameFlags = 0;
+        s->hitboxes[0].index = -1;
+        UpdateSpriteAnimation(s);
+    }
+
+    for(var_r7 = 0; var_r7 < 2; var_r7++)
+    {
+        Sprite *s = &config->spr15C[var_r7];
+        s->tiles = config->vram68;
+        if (var_r7 == 0) {
+            config->vram68 += gUnknown_080D7024[config->unk0].numTiles * TILE_SIZE_4BPP;
+            s->anim = gUnknown_080D7024[config->unk0].anim;
+            s->variant = gUnknown_080D7024[config->unk0].variant;
+        } else {
+            config->vram68 += gUnknown_080D7234[config->unk0].numTiles * TILE_SIZE_4BPP;
+            s->anim = gUnknown_080D7234[config->unk0].anim;
+            s->variant = gUnknown_080D7234[config->unk0].variant;
+        }
+        s->prevVariant = -1;
+        s->x = I(config->unk54);
+        s->y = I(config->unk58);
+        s->oamFlags = 0;
+        s->animCursor = 0;
+        s->qAnimDelay = 0;
+        s->animSpeed = 0x10;
+        s->palId = 0;
+        s->frameFlags = 0;
+        s->hitboxes[0].index = -1;
+        UpdateSpriteAnimation(s);
+    }
 }
 
+#if 0
 void Task_Options_ButtonConfig_Init(OptionsButtonConfig *config) {
     ButtonConfig *var_r7;
     s16 temp_r2;
@@ -364,17 +335,17 @@ void Task_Options_ButtonConfig_Init(OptionsButtonConfig *config) {
                 switch (temp_r0) {                  /* switch 1; irregular */
                 case 0:                             /* switch 1 */
                     var_r0 = *(temp_r1_4 + &gUnknown_080D73C0);
-                    var_r7 = &gLoadedSaveGame.buttonConfig;
+                    var_r7 = &LOADED_SAVE->buttonConfig;
 block_25:
                     var_r7->jump = var_r0;
                     break;
                 case 1:                             /* switch 1 */
                     var_r0 = *(temp_r1_4 + &gUnknown_080D73C0);
-                    var_r7 = (ButtonConfig *) &gLoadedSaveGame.buttonConfig.attack;
+                    var_r7 = (ButtonConfig *) &LOADED_SAVE->buttonConfig.attack;
                     goto block_25;
                 case 2:                             /* switch 1 */
                     var_r0 = *(temp_r1_4 + &gUnknown_080D73C0);
-                    var_r7 = (ButtonConfig *) &gLoadedSaveGame.buttonConfig.trick;
+                    var_r7 = (ButtonConfig *) &LOADED_SAVE->buttonConfig.trick;
                     goto block_25;
                 }
                 *(temp_r1 + 0xC + temp_r1_4) = *(temp_r6 + temp_r1_4);
@@ -404,13 +375,13 @@ block_25:
                 temp_r0_2 = *temp_r1_5;
                 switch (temp_r0_2) {                /* switch 2; irregular */
                 case 0:                             /* switch 2 */
-                    gLoadedSaveGame.buttonConfig.jump = *(temp_r2_3 + &gUnknown_080D73C0);
+                    LOADED_SAVE->buttonConfig.jump = *(temp_r2_3 + &gUnknown_080D73C0);
                     break;
                 case 1:                             /* switch 2 */
-                    gLoadedSaveGame.buttonConfig.attack = *(temp_r2_3 + &gUnknown_080D73C0);
+                    LOADED_SAVE->buttonConfig.attack = *(temp_r2_3 + &gUnknown_080D73C0);
                     break;
                 case 2:                             /* switch 2 */
-                    gLoadedSaveGame.buttonConfig.trick = *(temp_r2_3 + &gUnknown_080D73C0);
+                    LOADED_SAVE->buttonConfig.trick = *(temp_r2_3 + &gUnknown_080D73C0);
                     break;
                 }
                 var_r4_3 = (u16) (u8) (var_r4_3 + 1);
