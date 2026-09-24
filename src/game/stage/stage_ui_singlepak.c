a74a162b8a9e2dc180ad28f22259dd3e13271e99
JaceCear <11230293+JaceCear@users.noreply.github.com>
2026-09-24 22:16:26 +0200

    Formatter...

--- src/game/stage/stage_ui_singlepak.c
+++ src/game/stage/stage_ui_singlepak.c
@@ -35,12 +35,11 @@ void sub_8022B30(StageUiSinglePak *strc);
 extern const u8 gUnknown_080CE438[][2];
 extern const u8 gUnknown_080CE4B2[][2];
 // TODO: These palettes are probably identical to their idle animations, so maybe take from those directly in the future instead?
-ALIGNED(4) static const ColorRaw sCharacterPalettes[4][16] = {
-    [PLAYER_1] = INCPAL("graphics/stg_ui_singlepak_sonic.pal"),
-    [PLAYER_2] = INCPAL("graphics/stg_ui_singlepak_tails.pal"),
-    [PLAYER_3] = INCPAL("graphics/stg_ui_singlepak_knuckles.pal"),
-    [PLAYER_4] = INCPAL("graphics/stg_ui_singlepak_amy.pal")
-};
+ALIGNED(4)
+static const ColorRaw sCharacterPalettes[4][16] = { [PLAYER_1] = INCPAL("graphics/stg_ui_singlepak_sonic.pal"),
+                                                    [PLAYER_2] = INCPAL("graphics/stg_ui_singlepak_tails.pal"),
+                                                    [PLAYER_3] = INCPAL("graphics/stg_ui_singlepak_knuckles.pal"),
+                                                    [PLAYER_4] = INCPAL("graphics/stg_ui_singlepak_amy.pal") };
 
 #define VRAM_BASE_SINGLEPAK_UI (u8 *)(OBJ_VRAM0 + 0x2000)
 
